6626ad3482a492a061e3b0ad0ba674b27903a3a5
macvaz <none@none>
2008-10-01 09:19:52 +0000

    refs #266. Adapting EzWeb source code to django 1.0
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%401438

--- ezweb/management.py
+++ ezweb/management.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth import models as auth_app
 from django.contrib.auth.management import create_superuser
-from django.contrib.auth import models as auth_app 
-from django.db.models import signals
+from django.db.models.signals import post_syncdb
 
 # Para que no pregunte la creacion de usuario admin,
 # se crea automaticamente
-signals.post_syncdb.disconnect(create_superuser, sender=auth_app)
+
+post_syncdb.disconnect(sender=auth_app, dispatch_uid='django.contrib.auth.management.create_superuser')
