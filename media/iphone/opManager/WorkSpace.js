7cfb57d2a40496f8c70dc6f5c43202d707ad6622
jmartin <none@none>
2009-02-24 12:00:30 +0000

    refs #224. Bug persisting properties on unload solved. Bug loading Context variables solved.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%401662

--- media/iphone/opManager/WorkSpace.js
+++ media/iphone/opManager/WorkSpace.js
@@ -37,7 +37,6 @@ function WorkSpace (workSpaceState) {
 		this.workSpaceGlobalInfo = eval ('(' + response + ')');
 
 		this.varManager = new VarManager(this);
-		this.contextManager = new ContextManager(this, this.workSpaceGlobalInfo);
 		
 		var tabs = this.workSpaceGlobalInfo['workspace']['tabList'];
 
@@ -53,7 +52,7 @@ function WorkSpace (workSpaceState) {
 				}
 			}
 		}
-
+		this.contextManager = new ContextManager(this, this.workSpaceGlobalInfo);
 		this.wiring = new Wiring(this, this.workSpaceGlobalInfo);
 
 		this.loaded = true;
