77d56a15bcf9b4be2abc46d95288311e711f5675
pjm <none@none>
2009-05-22 08:19:04 +0000

    refs #207. new scripts for moving fixtures and restoring it when updating source code
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%401943

---
+++ scripts/restores_fixtures.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv connectable/fixtures/initial_data.xml2 connectable/fixtures/initial_data.xml
+mv gadget/fixtures/initial_data.xml2 gadget/fixtures/initial_data.xml
+mv igadget/fixtures/initial_data.xml2 igadget/fixtures/initial_data.xml
+mv catalogue/fixtures/initial_data.xml2 catalogue/fixtures/initial_data.xml
+mv workspace/fixtures/initial_data.xml2 workspace/fixtures/initial_data.xml
+mv context/fixtures/initial_data.xml2 context/fixtures/initial_data.xml
+mv ezweb/fixtures/initial_data.xml2 ezweb/fixtures/initial_data.xml
