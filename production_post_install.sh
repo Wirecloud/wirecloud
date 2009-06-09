021b9ff6008f513f60f981dd6baefca2acbb2d58
pjm <none@none>
2009-06-09 14:27:02 +0000

    refs #392. Generating a single versioned ezweb_[version].css file
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402109

--- production_post_install.sh
+++ production_post_install.sh
@@ -2,9 +2,11 @@
 # Post installation script. Optimization tasks for production environment.
 # Check 'settings.py' file for properties:
 #     ONLY_ONE_JS_FILE=True
+#     ONLY_ONE_CSS_FILE=True
 #     EZWEB_RELEASE='[_release_number]'
 # Release number can be obtained from SVN revision, executing 'svn info' or 'svnversion' commands
 
 set DJANGO_SETTINGS_MODULE=settings
 
 python generate_ezweb_js_file.py
+python generate_ezweb_css_file.py
