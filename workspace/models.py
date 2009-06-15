f2afbec4316aea0748f29c9db783946a5f4e892d
macvaz <none@none>
2009-06-15 12:22:37 +0000

    refs #396. Bigger lenght for storing email
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402152

--- workspace/models.py
+++ workspace/models.py
@@ -86,7 +86,7 @@ class PublishedWorkSpace(models.Model):
     description = models.TextField(_('Description'))
     
     author = models.CharField(_('Author'), max_length=250)
-    mail = models.CharField(_('Mail'), max_length=30)
+    mail = models.CharField(_('Mail'), max_length=100)
     
     #For implementing "private mashups" only visible for users that belongs to a concrete group
     organization = models.CharField(_('Organization'), max_length=80, null=True, blank=True)
