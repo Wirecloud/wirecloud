22c8d650f4b5e3c4968aca2dfeb1a637d6a064cc
Álvaro Arraanz <aarranz@yaco.es>
2009-06-26 10:28:58 +0000

    refs #286. Use textContent property instead of innerHTML for initializing style element content. Now WebKit loads the platform.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402285

--- media/js/opManager/OpManager.js
+++ media/js/opManager/OpManager.js
@@ -370,7 +370,7 @@ var OpManagerFactory = function () {
 				if(!BrowserUtilsFactory.getInstance().isIE()) {
 					var s = document.createElement('style');
 					s.type = "text/css";
-					s.innerHTML = '#wrapper { background-image: url('+_currentTheme.getIconURL('init-dat')+'); background-repeat: no-repeat; background-attachment:scroll; background-position: center bottom;}';
+					s.textContent = '#wrapper { background-image: url('+_currentTheme.getIconURL('init-dat')+'); background-repeat: no-repeat; background-attachment:scroll; background-position: center bottom;}';
 					var h = document.getElementsByTagName("head")[0];
 					h.appendChild(s);
 				}//TODO: for IE try: document.createStyleSheet() and addRule()
