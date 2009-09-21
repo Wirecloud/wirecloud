45ae73906aed8da1696327f26b3d503195c01c07
Álvaro Arraanz <aarranz@yaco.es>
2009-09-21 11:42:57 +0000

    refs #392. More fixes and optimizations on prototype.improvements.js.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402536

--- media/js/lib/prototype/prototype.improvements.js
+++ media/js/lib/prototype/prototype.improvements.js
@@ -38,27 +38,31 @@ Object.extend(Event, {
 	keyPressed: function(event) {
 		return Browser.isMSIE() ? window.event.keyCode : event.which;
 	}
-	/*
-	 * Event extension to manage user privileges 
-	 * */
-	observe: function(element, name, observer, useCapture, featureId){
-		var _observer = observer;
-		if (featureId && typeof(EzSteroidsAPI)!="undefined"){
-			//check the user policies
-			if (!EzSteroidsAPI.evaluePolicy(featureId)){
-				//if the user isn't allowed
-				_observer = function(msg){LogManagerFactory.getInstance().showMessage("You are not allowed to perform this operation");};
-			}
-		}
-		element = $(element);
-    	useCapture = useCapture || false;
-
-    	if (name == 'keypress' && (Prototype.Browser.WebKit || element.attachEvent))
-      		name = 'keydown';
-
-   		Event._observeAndCache(element, name, _observer, useCapture);
-	}
 });
+
+if (window.EzSteroidsAPI) {
+	// Backup prototype observe method
+	var _observe = Event.observe;
+
+	Object.extend(Event, {
+		// Old prototype observe method
+		_observe: _observe,
+
+		/*
+		 * Event extension to manage user privileges
+		 */
+		observe: function(element, name, observer, useCapture, featureId) {
+			if (featureId && !EzSteroidsAPI.evaluePolicy(featureId)) {
+				// If the user isn't allowed
+				observer = function(msg) {
+					var msg = gettext("You are not allowed to perform this operation");
+					LogManagerFactory.getInstance().showMessage(msg);
+				}
+			}
+			this._observe(element, name, observer);
+		}
+	});
+}
 
 var Browser = {
 	
