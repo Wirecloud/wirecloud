dc3721d96ad66ad24768d22c150c7f4ca06b290a
jmartin <none@none>
2009-09-22 14:54:59 +0000

    refs #276. settings repaired
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402545

--- settings.py
+++ settings.py
@@ -46,8 +46,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_OPTIONS = {"init_command": "SET storage_engine=InnoDB"}
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'ezweb'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'ezweb'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'ezweb'         # Not used with sqlite3.
