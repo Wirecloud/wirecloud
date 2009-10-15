66ee305e9330ad6ef9679cef1e6ca8b05c36eb99
Álvaro Arraanz <aarranz@yaco.es>
2009-10-15 10:40:42 +0000

    refs #276. Provide migration scripts.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402627

---
+++ scripts/load_tab_locked_prefs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+#...............................licence...........................................
+#
+#     (C) Copyright 2008 Telefonica Investigacion y Desarrollo
+#     S.A.Unipersonal (Telefonica I+D)
+#
+#     This file is part of Morfeo EzWeb Platform.
+#
+#     Morfeo EzWeb Platform is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Morfeo EzWeb Platform is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with Morfeo EzWeb Platform.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     Info about members and contributors of the MORFEO project
+#     is available at
+#
+#     http://morfeo-project.org
+#
+#...............................licence...........................................#
+
+from workspace.models import Tab
+from preferences.models import TabPreference
+
+mapping_file = open('mapping.txt', 'r')
+
+mapping_text = mapping_file.read()
+
+mapping_file.close()
+
+tabsLocked = eval(mapping_text)
+
+for tabId in tabsLocked:
+
+     tab = Tab.objects.get(id=tabId)
+
+     tabPreference = TabPreference(tab=tab, name="locked", value="1")
+     tabPreference.save()
