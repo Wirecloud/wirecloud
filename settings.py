e21bf30abdc4e3189dbb31a63aaa1b1b3039f150
Álvaro Arraanz <aarranz@yaco.es>
2009-10-27 09:52:30 +0000

    refs #514. Added a new template loader that looks for login, error 404 and error 500 templates on the default theme.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402659

--- settings.py
+++ settings.py
@@ -116,9 +116,7 @@ SECRET_KEY = '15=7f)g=)&spodi3bg8%&4fqt%f3rpg%b$-aer5*#a*(rqm79e'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'processors.template_loader.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -133,10 +131,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = (
-    path.join(BASEDIR, 'templates'),
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
