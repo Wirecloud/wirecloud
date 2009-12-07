c5f62460b7aa74232786d9d4ccc0ed4047a4ee2d
jmartin <none@none>
2009-12-07 09:54:20 +0000

    Fixes #538. The tab couldn't be removed due to an error in the dragboard accessing to methods of the layouts previously unloaded.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402719

--- media/js/dragboard/dragboard.js
+++ media/js/dragboard/dragboard.js
@@ -170,9 +170,12 @@ function Dragboard(tab, workSpace, dragboardElement) {
 	 * This method must only be used by <code>Tab</code>.
 	 */
 	Dragboard.prototype._notifyVisibilityChange = function (visibility) {
-		this.baseLayout._notifyDragboardVisibilityChange(visibility);
-		this.freeLayout._notifyDragboardVisibilityChange(visibility);
-		this.fulldragboardLayout._notifyDragboardVisibilityChange(visibility);
+		if (this.baseLayout)
+			this.baseLayout._notifyDragboardVisibilityChange(visibility);
+		if (this.freeLayout)
+			this.freeLayout._notifyDragboardVisibilityChange(visibility);
+		if (this.fulldragboardLayout)
+			this.fulldragboardLayout._notifyDragboardVisibilityChange(visibility);
 
 		if (!visibility) {
 			LayoutManagerFactory.getInstance().hideView(this.dragboardElement);
