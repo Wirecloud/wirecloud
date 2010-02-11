04c28593fcb73abac5bf3d64271dff3530a402c3
jmostazo-upm <none@none>
2010-02-11 09:16:58 +0000

    refs #563. Bug in wgt packages, encoding problem with some special characters y the gadget title
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%402778

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'ezweb'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'ezweb'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'ezweb'         # Not used with sqlite3.
