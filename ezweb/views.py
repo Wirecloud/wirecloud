6d9dae1ffc42c4549f4c1d0742a2c772f6384e04
Álvaro Arraanz <aarranz@yaco.es>
2010-10-06 08:02:36 +0000

    refs #211. Use simplejson module from django.
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%403045

--- ezweb/views.py
+++ ezweb/views.py
@@ -30,7 +30,7 @@
 
 #
 
-import urllib, simplejson, httplib, urlparse
+import urllib, httplib, urlparse
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
