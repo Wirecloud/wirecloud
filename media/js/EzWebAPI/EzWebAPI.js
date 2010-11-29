3699dfeab64edc6bae92f14e18db4fd6848ad990
Álvaro Arraanz <aarranz@yaco.es>
2010-11-29 08:30:20 +0000

    refs #187. Fix bug in EzWebAPI.buildProxyURL when using it with URLs that don't have path, for example "http://www.wikipedia.org"
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%403078

--- media/js/EzWebAPI/EzWebAPI.js
+++ media/js/EzWebAPI/EzWebAPI.js
@@ -91,6 +91,9 @@ _EzWebAPI.prototype.buildProxyURL = function(url, options) {
 	if (protocolEnd != -1) {
 		var domainStart = protocolEnd + 3;
 		var pathStart = url.indexOf('/', domainStart);
+		if (pathStart === -1) {
+			pathStart = url.length;
+		}
 
 		var protocol = url.substr(0, protocolEnd);
 		var domain = url.substr(domainStart, pathStart - domainStart);
