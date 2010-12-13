921b67dba09c12fb90540852044796cb4d942ff4
=?UTF-8?q?=C3=81lvaro=20Arranz=20Garc=C3=ADa?= <aarranz@yaco.es>
2010-12-13 18:16:27 +0100

    [PATCH 01/61] Use lxml when generating workspace templates so we can be sure we generate valid xml.
    From cd9ecf8ae858054aeb5612d8d12d15e3309846a5 Mon Sep 17 00:00:00 2001
    ---
     workspace/mashupTemplateGenerator.py |   66 +++++++++++++++-------------------
     1 files changed, 29 insertions(+), 37 deletions(-)

--- workspace/mashupTemplateGenerator.py
+++ workspace/mashupTemplateGenerator.py
@@ -31,6 +31,7 @@
 #
 
 from django.shortcuts import get_object_or_404
+from lxml import etree
 
 from igadget.models import IGadget
 from workspace.models import PublishedWorkSpace
@@ -44,59 +45,50 @@ class TemplateGenerator:
 
         included_igadgets = IGadget.objects.filter(tab__workspace=published_workspace.workspace)
 
-        xml = '<?xml version="1.0" encoding="utf-8"?>'
-        xml += '<Template schemaLocation="http://morfeo-project.org/2007/Template">'
-        xml += '<!-- Meta tags define gadgets properties -->'
-        xml += '<Catalog.ResourceDescription>'
-        xml += '<Vendor>%s</Vendor>' % published_workspace.vendor
-        xml += '<Name>%s</Name>' % published_workspace.name
-        xml += '<Version>%s</Version>' % published_workspace.version
-        xml += '<Author>%s</Author>' % published_workspace.author
-        xml += '<Mail>%s</Mail>' % published_workspace.mail
-        xml += '<Description>%s</Description>' % published_workspace.description
-        xml += '<ImageURI>%s</ImageURI>' % published_workspace.imageURI
-        xml += '<WikiURI>%s</WikiURI>' % published_workspace.wikiURI
-        xml += '<Organization>%s</Organization>' % published_workspace.organization
-        xml += '<IncludedResources mashupId="%s">' % workspace_id
+        template = etree.Element('Template', schemaLocation="http://morfeo-project.org/2007/Template")
+        desc = etree.Element('Catalog.ResourceDescription')
+        template.append(desc)
+        etree.SubElement(desc, 'Vendor').text = published_workspace.vendor
+        etree.SubElement(desc, 'Name').text = published_workspace.name
+        etree.SubElement(desc, 'Version').text = published_workspace.version
+        etree.SubElement(desc, 'Author').text = published_workspace.author
+        etree.SubElement(desc, 'Mail').text = published_workspace.mail
+        etree.SubElement(desc, 'Description').text = published_workspace.description
+        etree.SubElement(desc, 'ImageURI').text = published_workspace.imageURI
+        etree.SubElement(desc, 'WikiURI').text = published_workspace.wikiURI
+        etree.SubElement(desc, 'Organization').text = published_workspace.organization
 
-        wiring = ''
+        resources = etree.Element('IncludeResources', mashupId=workspace_id)
+        template.append(resources)
+
+        wiring = etree.Element('Platform.Wiring')
         contratable = False
 
         for igadget in included_igadgets:
             gadget = igadget.gadget
 
-            if (not contratable):
-                contratable = gadget.is_contratable()
+            contratable = contratable or gadget.is_contratable()
 
-            xml += '<Resource vendor="%s" name="%s" version="%s" />' % (gadget.vendor, gadget.name, gadget.version)
+            etree.Element('Resource', vendor=gadget.vendor, name=gadget.name, version=gadget.version)
 
             events = gadget.get_related_events()
 
             for event in events:
-                wiring += '<Event name="%s" type="%s" label="%s" friendcode="%s" />' % (event.name, event.type, event.label, event.friend_code)
+                wiring.append(etree.Element('Event', name=event.name, type=event.type, label=event.label, friendcode=event.friend_code))
 
             slots = gadget.get_related_slots()
 
             for slot in slots:
-                wiring += '<Slot name="%s" type="%s" label="%s" friendcode="%s" />' % (slot.name, slot.type, slot.label, slot.friend_code)
-
-        xml += '</IncludedResources>'
-        xml += '</Catalog.ResourceDescription>'
+                wiring.append(etree.Element('Slot', name=slot.name, type=slot.type, label=slot.label, friendcode=slot.friend_code))
 
         if contratable:
-            xml += '<Capability name="contratable" value="true"/>'
+            etree.append(etree.Element('Capability', name="contratable", value="true"))
 
-        xml += '<Platform.Preferences></Platform.Preferences>'
-        xml += '<Platform.StateProperties></Platform.StateProperties>'
+        preferences = etree.Element('Platform.Preferences')
+        template.append(preferences)
+        properties = etree.Element('Platform.StateProperties')
+        template.append(properties)
 
-        xml += '<Platform.Wiring>%s</Platform.Wiring>' % (wiring)
+        template.append(wiring)
 
-        xml += '<Platform.Link>'
-        xml += '<XHTML href=""/>'
-        xml += '</Platform.Link>'
-
-        xml += '<Platform.Rendering width="0" height="0"/>'
-
-        xml += '</Template>'
-
-        return xml
+        return etree.tostring(template, method='xml', xml_declaration=True, encoding="UTF-8", pretty_print=True)
