ce71dd331b8903ba3ab7fa62322c21eb74a67d31
Álvaro Arraanz <aarranz@yaco.es>
2011-03-01 12:34:45 +0000

    refs #752. Add migration data for the openid_auth application
    --HG--
    extra : convert_revision : svn%3A2a98c5bb-d66c-48ca-a96c-db028a6c31f4/ezweb_platform/src/trunk%403236

---
+++ openid_auth/django_openidconsumer/migrations/0001_initial.py
@@ -0,0 +1,60 @@
+# encoding: utf-8
+from south.db import db
+from south.v2 import SchemaMigration
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Adding model 'Nonce'
+        db.create_table('django_openidconsumer_nonce', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('server_url', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('timestamp', self.gf('django.db.models.fields.IntegerField')()),
+            ('salt', self.gf('django.db.models.fields.CharField')(max_length=40)),
+        ))
+        db.send_create_signal('django_openidconsumer', ['Nonce'])
+
+        # Adding model 'Association'
+        db.create_table('django_openidconsumer_association', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('server_url', self.gf('django.db.models.fields.TextField')(max_length=2047)),
+            ('handle', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('secret', self.gf('django.db.models.fields.TextField')(max_length=255)),
+            ('issued', self.gf('django.db.models.fields.IntegerField')()),
+            ('lifetime', self.gf('django.db.models.fields.IntegerField')()),
+            ('assoc_type', self.gf('django.db.models.fields.TextField')(max_length=64)),
+        ))
+        db.send_create_signal('django_openidconsumer', ['Association'])
+
+
+    def backwards(self, orm):
+
+        # Deleting model 'Nonce'
+        db.delete_table('django_openidconsumer_nonce')
+
+        # Deleting model 'Association'
+        db.delete_table('django_openidconsumer_association')
+
+
+    models = {
+        'django_openidconsumer.association': {
+            'Meta': {'object_name': 'Association'},
+            'assoc_type': ('django.db.models.fields.TextField', [], {'max_length': '64'}),
+            'handle': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'issued': ('django.db.models.fields.IntegerField', [], {}),
+            'lifetime': ('django.db.models.fields.IntegerField', [], {}),
+            'secret': ('django.db.models.fields.TextField', [], {'max_length': '255'}),
+            'server_url': ('django.db.models.fields.TextField', [], {'max_length': '2047'})
+        },
+        'django_openidconsumer.nonce': {
+            'Meta': {'object_name': 'Nonce'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'salt': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'server_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'timestamp': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['django_openidconsumer']
