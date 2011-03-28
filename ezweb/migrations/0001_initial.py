a22f10f4b4d38e14636b01b43395cc37563a2553
Álvaro Arranz García <aarranz@yaco.es>
2011-03-28 18:31:52 +0200

    ver #511. Fix fixture data dependency. no-pep8 no-pyflakes

---
+++ ezweb/migrations/0001_initial.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+from south.v2 import DataMigration
+
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+
+    }
+
+    complete_apps = ['ezweb']
