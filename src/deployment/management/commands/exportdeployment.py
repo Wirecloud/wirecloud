a918e3cc609141b8a7c1f29d617bf0a7c377e5c2
ablanco <ablanco@yaco.es>
2011-04-14 13:13:01 +0200

    Add two django commands for exporting/importing the deployment
    Add two django commands to EzWeb to export and import the gadgets from
    the deployment directory. The commands are:
    
     * exportdeployment
     * importdeployment
    
    Both of them accept only one argument, the target tar file.

---
+++ src/deployment/management/commands/exportdeployment.py
@@ -0,0 +1,46 @@
+# Copyright 2011 Yaco Sistemas <lgs@yaco.es>
+#
+# This file is part of EzWeb.
+
+# EzWeb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# EzWeb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with EzWeb.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+import tarfile
+
+
+class Command(BaseCommand):
+    args = '<target_file.tar.gz>'
+    help = 'Export the deployment directory in a tar.gz file'
+
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError('Wrong number of arguments')
+
+        if not args[0].endswith('tar.gz'):
+            raise CommandError('The target file must be a tar.gz file')
+
+        deployment_path = settings.GADGETS_DEPLOYMENT_DIR
+        if deployment_path.endswith('/'):
+            deployment_path = deployment_path[:-1]
+        deployment_path = deployment_path[:deployment_path.rindex('/')]
+        deployment_path = settings.GADGETS_DEPLOYMENT_DIR[
+            len(deployment_path) + 1:]
+
+        tar = tarfile.open(args[0], "w:gz")
+        tar.add(settings.GADGETS_DEPLOYMENT_DIR, arcname=deployment_path)
+        tar.close()
+
+        print 'Successfully exported deployment as "%s"\n' % args[0]
---
+++ src/deployment/management/commands/importdeployment.py
@@ -0,0 +1,42 @@
+# Copyright 2011 Yaco Sistemas <lgs@yaco.es>
+#
+# This file is part of EzWeb.
+
+# EzWeb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# EzWeb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with EzWeb.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+import tarfile
+
+
+class Command(BaseCommand):
+    args = '<file.tar.gz>'
+    help = 'Import a deployment directory from a tar file'
+
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError('Wrong number of arguments')
+
+        deployment_path = settings.GADGETS_DEPLOYMENT_DIR
+        if deployment_path.endswith('/'):
+            deployment_path = deployment_path[:-1]
+        deployment_path = deployment_path[:deployment_path.rindex('/')]
+        deployment_path = settings.GADGETS_DEPLOYMENT_DIR[:len(deployment_path)]
+
+        tar = tarfile.open(args[0], "r")
+        tar.extractall(deployment_path)
+        tar.close()
+
+        print 'Successfully imported deployment from "%s"\n' % args[0]
