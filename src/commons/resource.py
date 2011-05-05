ae374ba2d3ff9fc0df00948b5d8ba5c74b27a37b
Álvaro Arranz García <aarranz@yaco.es>
2011-05-05 16:55:15 +0200

    Really fix DELETE and PUT request timeouts

--- src/commons/resource.py
+++ src/commons/resource.py
@@ -88,7 +88,10 @@ class Resource:
         return HttpResponseServerError(get_json_error_response(msg), mimetype='application/json; charset=UTF-8')
 
     def adaptRequest(self, request):
+        real_method = request.method
+        request.method = 'POST'
         request._load_post_and_files()
+        request.method = real_method
 
         return request
 
