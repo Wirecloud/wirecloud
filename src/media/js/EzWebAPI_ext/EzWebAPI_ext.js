1db5d0c20711bcf9472fcccdba59b8f0ec50ab00
Álvaro Arranz García <aarranz@yaco.es>
2011-05-27 12:04:31 +0200

    Search for gadget-ie.css when using EzWebAPI_ext with IE

--- src/media/js/EzWebAPI_ext/EzWebAPI_ext.js
+++ src/media/js/EzWebAPI_ext/EzWebAPI_ext.js
@@ -323,6 +323,11 @@ if (!window.preventDefaultStyle) {
     try {
         /* TODO */
         var theme = window.parent.URIs.ACTIVE_THEME;
+
+        if (EzWebExt.Browser.isIE()) {
+            EzWebExt.prependStyle(EzWebAPI.platform_domain + "/ezweb/themes/" + encodeURIComponent(theme.getValue()) + "/gadgets/gadget-ie.css");
+        }
+        
         EzWebExt.prependStyle(EzWebAPI.platform_domain + theme + "/css/gadget.css");
     } catch (e) {}
 
