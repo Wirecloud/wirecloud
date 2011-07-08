4175fafb8956832afaa5966311e15a69ee65ebde
Álvaro Arraanz <aarranz@yaco.es>
2011-07-08 08:40:49 +0200

    Fix style in response_command_dispatcher.js and user_command_manager.js at ezweb/static/js/catalogue/views to make them pass jslint

--- src/ezweb/static/js/catalogue/views/response_command_dispatcher.js
+++ src/ezweb/static/js/catalogue/views/response_command_dispatcher.js
@@ -23,6 +23,10 @@
 *     http://morfeo-project.org
  */
 
+/*jslint white: true, onevar: true, undef: true, nomen: false, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global alert, Hash, ShowcaseFactory */
+"use strict";
+
 var ResponseCommandDispatcher = function (dom_wrapper, user_command_manager) {
     this.dom_wrapper = dom_wrapper;
     this.user_command_manager = user_command_manager;
@@ -37,21 +41,23 @@ ResponseCommandDispatcher.prototype.set_painter = function (painter_code, painte
 };
 
 ResponseCommandDispatcher.prototype.init = function () {
+    var element;
+
     //PAINTERS
-    var pagination_div = this.dom_wrapper.get_element_by_code('PAGINATION_AREA');
-    this.painters['PAGINATION_PAINTER'].set_dom_element(pagination_div);
+    element = this.dom_wrapper.get_element_by_code('PAGINATION_AREA');
+    this.painters.PAGINATION_PAINTER.set_dom_element(element);
 
-    var gadgets_div = this.dom_wrapper.get_element_by_code('GADGET_LIST');
-    this.painters['GADGETS_PAINTER'].set_dom_element(gadgets_div);
+    element = this.dom_wrapper.get_element_by_code('GADGET_LIST');
+    this.painters.GADGETS_PAINTER.set_dom_element(element);
 
-    var mashups_div = this.dom_wrapper.get_element_by_code('MASHUP_LIST');
-    this.painters['MASHUPS_PAINTER'].set_dom_element(mashups_div);
+    element = this.dom_wrapper.get_element_by_code('MASHUP_LIST');
+    this.painters.MASHUPS_PAINTER.set_dom_element(element);
 
-    var developers_div = this.dom_wrapper.get_element_by_code('DEVELOPER_INFO_AREA');
-    this.painters['DEVELOPERS_PAINTER'].set_dom_element(developers_div);
+    element = this.dom_wrapper.get_element_by_code('DEVELOPER_INFO_AREA');
+    this.painters.DEVELOPERS_PAINTER.set_dom_element(element);
 
-    var resource_details_div = this.dom_wrapper.get_element_by_code('RESOURCE_DETAILS_AREA');
-    this.painters['RESOURCE_DETAILS_PAINTER'].set_dom_element(resource_details_div);
+    element = this.dom_wrapper.get_element_by_code('RESOURCE_DETAILS_AREA');
+    this.painters.RESOURCE_DETAILS_PAINTER.set_dom_element(element);
 
     // TOOLBAR BUTTONS
     this.search_button = this.dom_wrapper.get_element_by_code('SEARCH_TOOLBAR_BUTTON');
@@ -64,87 +70,94 @@ ResponseCommandDispatcher.prototype.init = function () {
 };
 
 ResponseCommandDispatcher.prototype.process = function (resp_command) {
-    var display_options = {
-        'search_options': 'none', 'pagination':'none', 'developer_info': 'none',
-        'gadget_list':'none', 'mashup_list':'none', 'resource_details':'none',
-        'tab_bar': 'none'};
+    var display_options, services, command_id, command_data;
 
-    var services = this.user_command_manager.get_service_facade();
-    var command_id = resp_command.get_id();
+    display_options = {
+        'search_options': 'none',
+        'pagination': 'none',
+        'developer_info': 'none',
+        'gadget_list': 'none',
+        'mashup_list': 'none',
+        'resource_details': 'none',
+        'tab_bar': 'none'
+    };
+
+    services = this.user_command_manager.get_service_facade();
+    command_id = resp_command.get_id();
 
     switch (command_id) {
     case 'PAINT_GADGETS':
     case 'SHOW_GADGETS':
-        display_options['search_options'] = 'block';
-        display_options['pagination'] = 'block';
-        display_options['gadget_list'] = 'block';
-        display_options['tab_bar'] = 'block';
+        display_options.search_options = 'block';
+        display_options.pagination = 'block';
+        display_options.gadget_list = 'block';
+        display_options.tab_bar = 'block';
 
         this.show_section(display_options, command_id);
 
-        if (command_id == 'PAINT_GADGETS') {
-            this.painters['GADGETS_PAINTER'].paint(resp_command, this.user_command_manager);
+        if (command_id === 'PAINT_GADGETS') {
+            this.painters.GADGETS_PAINTER.paint(resp_command, this.user_command_manager);
             ShowcaseFactory.getInstance().setGadgetsState(resp_command.data.resources);
         }
 
-        this.painters['PAGINATION_PAINTER'].paint(resp_command, this.user_command_manager);
+        this.painters.PAGINATION_PAINTER.paint(resp_command, this.user_command_manager);
 
         break;
     case 'PAINT_MASHUPS':
     case 'SHOW_MASHUPS':
-        display_options['search_options'] = 'block';
-        display_options['pagination'] = 'block';
-        display_options['mashup_list'] = 'block';
-        display_options['tab_bar'] = 'block';
+        display_options.search_options = 'block';
+        display_options.pagination = 'block';
+        display_options.mashup_list = 'block';
+        display_options.tab_bar = 'block';
 
         this.show_section(display_options, command_id);
 
-        if (command_id == 'PAINT_MASHUPS') {
-            this.painters['MASHUPS_PAINTER'].paint(resp_command, this.user_command_manager);
+        if (command_id === 'PAINT_MASHUPS') {
+            this.painters.MASHUPS_PAINTER.paint(resp_command, this.user_command_manager);
         }
 
-        this.painters['PAGINATION_PAINTER'].paint(resp_command, this.user_command_manager);
+        this.painters.PAGINATION_PAINTER.paint(resp_command, this.user_command_manager);
 
         break;
     case 'PAINT_RESOURCE_DETAILS':
-        display_options['resource_details'] = 'block';
+        display_options.resource_details = 'block';
 
         this.show_section(display_options, command_id);
 
-        this.painters['RESOURCE_DETAILS_PAINTER'].paint(resp_command, this.user_command_manager);
+        this.painters.RESOURCE_DETAILS_PAINTER.paint(resp_command, this.user_command_manager);
 
         break;
     case 'SHOW_DEVELOPER_INFO':
-        display_options['developer_info'] = 'block';
+        display_options.developer_info = 'block';
 
         this.show_section(display_options, command_id);
 
-        this.painters['DEVELOPERS_PAINTER'].paint(resp_command, this.user_command_manager);
+        this.painters.DEVELOPERS_PAINTER.paint(resp_command, this.user_command_manager);
 
         break;
     case 'SHOW_SEARCH_INFO':
-        display_options['search_options'] = 'block';
-        display_options['pagination'] = 'block';
-        display_options['tab_bar'] = 'block';
+        display_options.search_options = 'block';
+        display_options.pagination = 'block';
+        display_options.tab_bar = 'block';
 
-        var command_data = resp_command.get_data();
+        command_data = resp_command.get_data();
 
-        if (command_data == 'gadget')
-          display_options['gadget_list'] = 'block';
-
-        if (command_data == 'mashup')
-          display_options['mashup_list'] = 'block';
+        if (command_data === 'gadget') {
+            display_options.gadget_list = 'block';
+        } else if (command_data === 'mashup') {
+            display_options.mashup_list = 'block';
+        }
 
         this.show_section(display_options, command_id);
 
         break;
     case 'SUBMIT_GADGET':
     case 'SUBMIT_PACKAGED_GADGET':
-        display_options['developer_info'] = 'block';
+        display_options.developer_info = 'block';
 
         this.show_section(display_options, command_id);
 
-        this.painters['DEVELOPERS_PAINTER'].paint_adding_gadget_results(resp_command, this.user_command_manager);
+        this.painters.DEVELOPERS_PAINTER.paint_adding_gadget_results(resp_command, this.user_command_manager);
         break;
     case 'REPEAT_SEARCH':
         services.repeat_last_search();
@@ -157,13 +170,13 @@ ResponseCommandDispatcher.prototype.process = function (resp_command) {
 };
 
 ResponseCommandDispatcher.prototype.show_section = function (display_options, command_id) {
-    var search_options = display_options['search_options'];
-    var pagination = display_options['pagination'];
-    var gadget_list = display_options['gadget_list'];
-    var mashup_list = display_options['mashup_list'];
-    var resource_details = display_options['resource_details'];
-    var developer_info = display_options['developer_info'];
-    var tab_bar = display_options['tab_bar'];
+    var search_options = display_options.search_options,
+        pagination = display_options.pagination,
+        gadget_list = display_options.gadget_list,
+        mashup_list = display_options.mashup_list,
+        resource_details = display_options.resource_details,
+        developer_info = display_options.developer_info,
+        tab_bar = display_options.tab_bar;
 
 
     this.dom_wrapper.get_element_by_code('SEARCH_OPTIONS_AREA').setStyle({'display': search_options});
@@ -210,6 +223,6 @@ ResponseCommandDispatcher.prototype.show_section = function (display_options, co
         this.gadgets_button.removeClassName('current');
         break;
     default:
-        alert ('Error activating tab!');
+        alert('Error activating tab!');
     }
 };
--- src/ezweb/static/js/catalogue/views/user_command_manager.js
+++ src/ezweb/static/js/catalogue/views/user_command_manager.js
@@ -23,6 +23,10 @@
 *     http://morfeo-project.org
  */
 
+/*jslint white: true, onevar: true, undef: true, nomen: false, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global alert, ChangeResourceVersionCommand, DeleteResourceCommand, InstantiateCommand, RemoveResourceTagCommand, ShowResourceDetailsCommand, ShowResourceListCommand, ShowTabCommand, ShowToolbarSectionCommand, ShowWindowCommand, SimpleSearchCommand, SubmitGadgetCommand, SubmitPackagedGadgetCommand, TagResourceCommand, UpdateResourceHTMLCommand, ViewAllCommand, VoteResourceCommand */
+"use strict";
+
 var UserCommandManager = function (dom_wrapper) {
 
     this.catalogue = null;
@@ -33,44 +37,53 @@ var UserCommandManager = function (dom_wrapper) {
 
     this.set_commands_info = function (commands_info) {
         this.commands_info = commands_info;
-    }
+    };
 
     this.set_catalogue = function (catalogue) {
         this.catalogue = catalogue;
-    }
+    };
 
     this.set_services = function (services) {
         this.services = services;
-    }
+    };
 
     this.init = function (dom_element) {
         this.register_commands(dom_element);
-    }
+    };
 
     this.register_commands = function (dom_element) {
-        var command_keys = this.commands_info.keys();
+        var command_keys, i, element_selector, command_code, element;
 
-        for (var i=0; i<command_keys.length; i++) {
-            var element_selector = command_keys[i];
-            var command_code = this.commands_info[element_selector];
+        command_keys = this.commands_info.keys();
 
-            var element = this.dom_wrapper.search_by_selector(element_selector);
+        for (i = 0; i < command_keys.length; i += 1) {
+            element_selector = command_keys[i];
+            command_code = this.commands_info[element_selector];
+
+            element = this.dom_wrapper.search_by_selector(element_selector);
 
             this.create_general_command(command_code, element);
         }
-    }
+    };
 
     this.create_general_command = function (command_id, dom_element) {
+        var html_event, command, element_code, data;
+
         // Default html event = click
-        var html_event = 'click';
+        html_event = 'click';
 
         switch (command_id) {
         case 'VIEW_ALL':
-            var command = new ViewAllCommand(dom_element, html_event, this.services, this.dom_wrapper);
+            command = new ViewAllCommand(dom_element, html_event, this.services, this.dom_wrapper);
             break;
         case 'SIMPLE_SEARCH':
-            var element_code = this.dom_wrapper.get_code_by_element(dom_element);
-            var data = {'starting_page': 1, 'scope': '', 'boolean_operator': 'AND', 'criteria': ''};
+            element_code = this.dom_wrapper.get_code_by_element(dom_element);
+            data = {
+                'starting_page': 1,
+                'scope': '',
+                'boolean_operator': 'AND',
+                'criteria': ''
+            };
 
             switch (element_code) {
             case 'SEARCH_INPUT':
@@ -82,86 +95,87 @@ var UserCommandManager = function (dom_wrapper) {
                 break;
             case 'GADGETS_BUTTON':
                 html_event = 'click';
-                data['scope'] = 'gadget';
+                data.scope = 'gadget';
                 break;
             case 'MASHUPS_BUTTON':
                 html_event = 'click';
-                data['scope'] = 'mashup';
+                data.scope = 'mashup';
                 break;
             }
 
-            var command = new SimpleSearchCommand(dom_element, html_event, this.services, this.dom_wrapper, data);
+            command = new SimpleSearchCommand(dom_element, html_event, this.services, this.dom_wrapper, data);
             break;
         case 'SHOW_DEVELOPER_INFO':
         case 'SHOW_SEARCH_INFO':
-            var command = new ShowToolbarSectionCommand(dom_element, html_event, this.services, this.dom_wrapper, command_id);
+            command = new ShowToolbarSectionCommand(dom_element, html_event, this.services, this.dom_wrapper, command_id);
             break;
         case 'SHOW_GADGETS':
         case 'SHOW_MASHUPS':
-            var command = new ShowTabCommand(dom_element, html_event, this.services, this.dom_wrapper, command_id);
+            command = new ShowTabCommand(dom_element, html_event, this.services, this.dom_wrapper, command_id);
             break;
         default:
             return alert('event not identified! ' + command_id);
         }
-    }
+    };
 
     this.create_command_from_data = function (command_id, dom_element, command_data, html_event) {
-        // Default html event = click
-        var default_html_event = 'click';
+        var command;
 
-        if (!html_event)
-            html_event = default_html_event;
+        // Default html event = click
+        if (!html_event) {
+            html_event = 'click';
+        }
 
         switch (command_id) {
         case 'INSTANTIATE_RESOURCE':
-            var command = new InstantiateCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new InstantiateCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SIMPLE_SEARCH':
-            var command = new SimpleSearchCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new SimpleSearchCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SHOW_RESOURCE_DETAILS':
-            var command = new ShowResourceDetailsCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new ShowResourceDetailsCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SUBMIT_GADGET':
-            var command = new SubmitGadgetCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new SubmitGadgetCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SUBMIT_PACKAGED_GADGET':
-            var command = new SubmitPackagedGadgetCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new SubmitPackagedGadgetCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SHOW_WINDOW':
-            var command = new ShowWindowCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new ShowWindowCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'SHOW_RESOURCE_LIST':
-            var command = new ShowResourceListCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data);
+            command = new ShowResourceListCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data);
             break;
         case 'DELETE_RESOURCE':
-            var command = new DeleteResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new DeleteResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'UPDATE_RESOURCE':
-            var command = new UpdateResourceHTMLCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new UpdateResourceHTMLCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'VOTE_RESOURCE':
-            var command = new VoteResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new VoteResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'CHANGE_RESOURCE_VERSION':
-            var command = new ChangeResourceVersionCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new ChangeResourceVersionCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'TAG_RESOURCE':
-            var command = new TagResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new TagResourceCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         case 'DELETE_TAG':
-            var command = new RemoveResourceTagCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
+            command = new RemoveResourceTagCommand(dom_element, html_event, this.services, this.dom_wrapper, command_data, command_id);
             break;
         default:
             return alert('event not identified! ' + command_id);
         }
-    }
+    };
 
     this.get_service_facade = function () {
         return this.services;
-    }
+    };
 
     this.run_initial_commands = function () {
         this.services.search('VIEW_ALL', 1, 'AND', '', '');
-    }
-}
+    };
+};
