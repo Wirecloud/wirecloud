15f2dae5c0deafd4aec82cbd41a1a03fb65e370a
Álvaro Arranz García <aarranz@conwet.com>
2012-06-08 12:29:57 +0200

    Fix some problems in the wirecloud templatetags code

--- src/ezweb/templatetags/wirecloud.py
+++ src/ezweb/templatetags/wirecloud.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2011 Universidad Politécnica de Madrid
+# Copyright 2011-2012 Universidad Politécnica de Madrid
 
-# This file is part of EzWeb.
+# This file is part of Wirecluod.
 
-# EzWeb is free software: you can redistribute it and/or modify
+# Wirecloud is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# EzWeb is distributed in the hope that it will be useful,
+# Wirecloud is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with EzWeb.  If not, see <http://www.gnu.org/licenses/>.
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
 
 from django import template
-from django.utils.safestring import mark_safe
 
 from ezweb.plugins import get_extra_javascripts, get_wirecloud_ajax_endpoints
 
 
 register = template.Library()
+
 
 def extra_javascripts(context, view):
     files = get_extra_javascripts(view)
