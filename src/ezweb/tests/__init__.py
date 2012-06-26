2597a31e8e932af57c03f054169c611b7146752c
Álvaro Arranz García <aarranz@conwet.com>
2012-06-26 10:42:11 +0200

    Move some test from ezweb to wirecloud

--- src/ezweb/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from proxy.tests import ProxyTests, ProxySecureDataTests
-from ezweb.tests.plugins import WirecloudPluginTestCase
-
-from commons.test import build_selenium_test_cases
-
-build_selenium_test_cases(('ezweb.tests.selenium_tests.BasicSeleniumTests',), locals())
