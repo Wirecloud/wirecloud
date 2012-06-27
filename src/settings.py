62a02f10474fd356fafc9f16daa2853759044954
Álvaro Arranz García <aarranz@conwet.com>
2012-06-28 01:14:46 +0200

    Change default database configuration

--- src/settings.py
+++ src/settings.py
@@ -53,12 +53,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': path.join(BASEDIR, 'ezweb.db'),  # Or path to database file if using sqlite3.
-        'USER': '',                              # Not used with sqlite3.
-        'PASSWORD': '',                          # Not used with sqlite3.
-        'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                              # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(BASEDIR, 'wirecloud.db'),  # Or path to database file if using sqlite3.
+        'TEST_NAME': path.join(BASEDIR, 'test_wirecloud.db'),
+        'USER': '',                                  # Not used with sqlite3.
+        'PASSWORD': '',                              # Not used with sqlite3.
+        'HOST': '',                                  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                  # Set to empty string for default. Not used with sqlite3.
     },
 }
 
