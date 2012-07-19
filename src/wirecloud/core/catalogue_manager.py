0a372a0fcb44065137c5cfc31433cacc7e8f08e8
Álvaro Arranz García <aarranz@conwet.com>
2012-07-20 00:02:17 +0200

    Add copyright headers to some files

--- src/wirecloud/core/catalogue_manager.py
+++ src/wirecloud/core/catalogue_manager.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
 from catalogue.utils import add_resource_from_template
 from commons.template import TemplateParser
 from workspace.mashupTemplateGenerator import build_template_from_workspace
