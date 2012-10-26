63d1d9c5e9e4e1499ad2fc57a5630c5b16aaff0a
Álvaro Arranz García <aarranz@conwet.com>
2012-10-26 16:21:47 +0200

    Fix jshint problems in Select.js

--- src/wirecloudcommons/static/js/StyledElements/Select.js
+++ src/wirecloudcommons/static/js/StyledElements/Select.js
@@ -1,160 +1,174 @@
-/**
- *
- * Options:
- *     * initialEntries:
- *     * initialValue:
- *     * idFunc: In case you want to assign non-string values, you must provide
- *     a function for converting them into strings.
- */
-StyledElements.StyledSelect = function(options) {
-    options = EzWebExt.merge({
-        'class': '',
-        'initialEntries': [],
-        'initialValue': null,
-        'idFunc': function (value) {
-            if (typeof value === 'string') {
-                return value;
-            } else if (value === null || value === undefined) {
-                return '';
-            } else if (typeof value === 'number') {
-                return '' + value;
-            } else {
-                throw new TypeError();
+/*globals EzWebExt, StyledElements*/
+
+(function () {
+
+    "use strict";
+
+    /**
+     *
+     * Options:
+     *     * initialEntries:
+     *     * initialValue:
+     *     * idFunc: In case you want to assign non-string values, you must provide
+     *     a function for converting them into strings.
+     */
+    var StyledSelect = function StyledSelect(options) {
+        options = EzWebExt.merge({
+            'class': '',
+            'initialEntries': [],
+            'initialValue': null,
+            'idFunc': function (value) {
+                if (typeof value === 'string') {
+                    return value;
+                } else if (value === null || value === undefined) {
+                    return '';
+                } else if (typeof value === 'number') {
+                    return '' + value;
+                } else {
+                    throw new TypeError();
+                }
+            }
+        },
+        options);
+
+        StyledElements.StyledInputElement.call(this, options.initialValue, ['change']);
+
+        this.wrapperElement = document.createElement("div");
+        this.wrapperElement.className = EzWebExt.prependWord(options['class'], "styled_select");
+
+        var div =  document.createElement("div");
+        div.className = "arrow";
+        this.inputElement = document.createElement("select");
+
+        if (options.name) {
+            this.inputElement.setAttribute("name", options.name);
+        }
+
+        if (options.id) {
+            this.wrapperElement.setAttribute("id", options.id);
+        }
+
+        this.textDiv = document.createElement("div");
+        this.textDiv.className = "text";
+
+        this.optionsByValue = {};
+        this.optionValues = {};
+        this.idFunc = options.idFunc;
+        this.addEntries(options.initialEntries);
+
+        EzWebExt.addEventListener(this.inputElement, "change",
+                                    EzWebExt.bind(function (event) {
+                                        if (this.enabled) {
+                                            var optionList = event.target;
+                                            EzWebExt.setTextContent(this.textDiv, optionList[optionList.selectedIndex].text);
+                                            this.events.change.dispatch(this);
+                                        }
+                                    }, this),
+                                    true);
+
+        this.wrapperElement.appendChild(this.textDiv);
+        this.wrapperElement.appendChild(div);
+        this.wrapperElement.appendChild(this.inputElement);
+
+        // initialize the textDiv with the initial selection
+        var selectedIndex = this.inputElement.options.selectedIndex;
+        if (selectedIndex !== -1)
+            EzWebExt.setTextContent(this.textDiv, this.inputElement.options[selectedIndex].text);
+    };
+    StyledSelect.prototype = new StyledElements.StyledInputElement();
+
+    StyledSelect.prototype.getLabel = function getLabel() {
+        return EzWebExt.getTextContent(this.textDiv);
+    };
+
+    StyledSelect.prototype.getValue = function getValue() {
+        return this.optionValues[this.inputElement.value];
+    };
+
+    StyledSelect.prototype.setValue = function setValue(newValue) {
+        if (typeof newValue !== 'string') {
+            try {
+                newValue = this.idFunc(newValue);
+            } catch (e) {
+                newValue = null;
             }
         }
-    },
-    options);
 
-    StyledElements.StyledInputElement.call(this, options['initialValue'], ['change']);
-
-    this.wrapperElement = document.createElement("div");
-    this.wrapperElement.className = EzWebExt.prependWord(options['class'], "styled_select");
-
-    var div =  document.createElement("div");
-    div.className = "arrow";
-    this.inputElement = document.createElement("select");
-
-    if (options['name'])
-        this.inputElement.setAttribute("name", options['name']);
-
-    if (options['id'])
-        this.wrapperElement.setAttribute("id", options['id']);
-
-    this.textDiv = document.createElement("div");
-    this.textDiv.className = "text";
-
-    this.optionsByValue = {};
-    this.optionValues = {};
-    this.idFunc = options.idFunc;
-    this.addEntries(options['initialEntries']);
-
-    EzWebExt.addEventListener(this.inputElement, "change",
-                                EzWebExt.bind(function(event) {
-                                    if (this.enabled) {
-                                        var optionList = event.target;
-                                        EzWebExt.setTextContent(this.textDiv, optionList[optionList.selectedIndex].text);
-                                        this.events['change'].dispatch(this);
-                                    }
-                                }, this),
-                                true);
-
-    this.wrapperElement.appendChild(this.textDiv);
-    this.wrapperElement.appendChild(div);
-    this.wrapperElement.appendChild(this.inputElement);
-
-    // initialize the textDiv with the initial selection
-    var selectedIndex = this.inputElement.options.selectedIndex;
-    if (selectedIndex !== -1)
-        EzWebExt.setTextContent(this.textDiv, this.inputElement.options[selectedIndex].text);
-}
-StyledElements.StyledSelect.prototype = new StyledElements.StyledInputElement();
-
-StyledElements.StyledSelect.prototype.getLabel = function () {
-    return EzWebExt.getTextContent(this.textDiv);
-}
-
-StyledElements.StyledSelect.prototype.getValue = function () {
-    return this.optionValues[this.inputElement.value];
-}
-
-StyledElements.StyledSelect.prototype.setValue = function (newValue) {
-    if (typeof newValue !== 'string') {
-        try {
-            newValue = this.idFunc(newValue);
-        } catch (e) {
-            newValue = null;
+        // TODO exception if the newValue is not listened in the option list?
+        if (newValue === null || !(newValue in this.optionValues)) {
+            if (this.defaultValue != null) {
+                newValue = this.defaultValue;
+            } else if (this.inputElement.options.length > 0) {
+                newValue = this.inputElement.options[0].value;
+            } else {
+                StyledElements.StyledInputElement.prototype.setValue.call(this, '');
+                EzWebExt.setTextContent(this.textDiv, '');
+                return;
+            }
         }
-    }
 
-    // TODO exception if the newValue is not listened in the option list?
-    if (newValue === null || !(newValue in this.optionValues)) {
-        if (this.defaultValue != null) {
-            newValue = this.defaultValue;
-        } else if (this.inputElement.options.length > 0) {
-            newValue = this.inputElement.options[0].value;
-        } else {
-            StyledElements.StyledInputElement.prototype.setValue.call(this, '');
-            EzWebExt.setTextContent(this.textDiv, '');
+        StyledElements.StyledInputElement.prototype.setValue.call(this, newValue);
+        EzWebExt.setTextContent(this.textDiv, this.optionsByValue[newValue]);
+    };
+
+    /**
+     * @param {null|Array} newEntries Entries to add. This method does nothing if
+     * newEntries is null.
+     */
+    StyledSelect.prototype.addEntries = function addEntries(newEntries) {
+        var oldSelectedIndex, optionValue, optionLabel;
+
+        oldSelectedIndex = this.inputElement.options.selectedIndex;
+
+        if (newEntries == null || newEntries.length === 0) {
             return;
         }
-    }
 
-    StyledElements.StyledInputElement.prototype.setValue.call(this, newValue);
-    EzWebExt.setTextContent(this.textDiv, this.optionsByValue[newValue]);
-}
+        for (var i = 0; i < newEntries.length; i++) {
+            var option = document.createElement("option");
+            if (newEntries[i] instanceof Array) {
+                optionValue = newEntries[i][0];
+                optionLabel = newEntries[i][1];
+            } else {
+                optionValue = newEntries[i].value;
+                optionLabel = newEntries[i].label;
+            }
+            optionLabel = optionLabel ? optionLabel : optionValue;
 
-/**
- * @param {null|Array} newEntries Entries to add. This method does nothing if 
- * newEntries is null.
- */
-StyledElements.StyledSelect.prototype.addEntries = function (newEntries) {
-    var oldSelectedIndex = this.inputElement.options.selectedIndex;
+            var realValue = optionValue;
+            if (typeof optionValue !== 'string') {
+                optionValue = this.idFunc(optionValue);
+            }
+            option.setAttribute("value", optionValue);
+            option.appendChild(document.createTextNode(optionLabel));
 
-    if (newEntries == null || newEntries.length == 0)
-        return;
+            if (this.defaultValue == optionValue) {
+                option.setAttribute("selected", "selected");
+            }
 
-    for (var i = 0; i < newEntries.length; i++) {
-        var option = document.createElement("option");
-        if (newEntries[i] instanceof Array) {
-            optionValue = newEntries[i][0];
-            optionLabel = newEntries[i][1];
-        } else {
-            optionValue = newEntries[i].value;
-            optionLabel = newEntries[i].label;
-        }
-        optionLabel = optionLabel ? optionLabel : optionValue;
-
-        var realValue = optionValue;
-        if (typeof optionValue !== 'string') {
-            optionValue = this.idFunc(optionValue);
-        }
-        option.setAttribute("value", optionValue);
-        option.appendChild(document.createTextNode(optionLabel));
-
-        if (this.defaultValue == optionValue) {
-            option.setAttribute("selected", "selected");
+            this.inputElement.appendChild(option);
+            this.optionValues[optionValue] = realValue;
+            this.optionsByValue[optionValue] = optionLabel;
         }
 
-        this.inputElement.appendChild(option);
-        this.optionValues[optionValue] = realValue;
-        this.optionsByValue[optionValue] = optionLabel;
-    }
+        // initialize the textDiv with the initial selection
+        var selectedIndex = this.inputElement.options.selectedIndex;
+        if (oldSelectedIndex !== selectedIndex) {
+            EzWebExt.setTextContent(this.textDiv, this.inputElement.options[selectedIndex].text);
+        }
+    };
 
-    // initialize the textDiv with the initial selection
-    var selectedIndex = this.inputElement.options.selectedIndex;
-    if (oldSelectedIndex !== selectedIndex)
-        EzWebExt.setTextContent(this.textDiv, this.inputElement.options[selectedIndex].text);
-}
+    StyledSelect.prototype.clear = function clear() {
+        // Clear textDiv
+        EzWebExt.setTextContent(this.textDiv, "");
 
-StyledElements.StyledSelect.prototype.clear = function () {
-    // Clear textDiv
-    EzWebExt.setTextContent(this.textDiv, "");
+        // Clear select element options
+        EzWebExt.setTextContent(this.inputElement, "");
 
-    // Clear select element options
-    EzWebExt.setTextContent(this.inputElement, "");
+        this.optionsByValue = {};
+        this.optionsValues = {};
+    };
 
-    this.optionsByValue = {};
-    this.optionsValues = {};
-}
+    StyledElements.StyledSelect = StyledSelect;
 
-
+})();
