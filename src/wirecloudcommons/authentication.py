9c4d1dc312c5d1af5edc509cb7ec6e49cc03df7e
Álvaro Arranz García <aarranz@conwet.com>
2012-11-15 23:09:46 +0100

    Simplify logout

--- src/wirecloudcommons/authentication.py
+++ src/wirecloudcommons/authentication.py
@@ -17,19 +17,15 @@
 # You should have received a copy of the GNU General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext as _
 from django.conf import settings
 
 
 def logout(request, next_page=settings.LOGOUT_URL, template_name='registration/logged_out.html'):
-    "Logs out the user and displays 'You are logged out' message."
-    if 'django_language' in request.session:
-        old_lang = request.session['django_language']
-    else:
-        old_lang = None
+
+    old_lang = request.session.get('django_language', None)
 
     request.session.flush()
 
@@ -37,7 +33,7 @@ def logout(request, next_page=settings.LOGOUT_URL, template_name='registration/l
         request.session['django_language'] = old_lang
 
     if next_page is None:
-        return render_to_response(template_name, {'title': _('Logged out')}, context_instance=RequestContext(request))
+        return render(request, template_name, {'title': _('Logged out')})
     else:
         # Redirect to this page until the session has been cleared.
         return HttpResponseRedirect(next_page or request.path)
