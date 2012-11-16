6e753d2e14b2f83b608d05358be2b88c548f1711
Álvaro Arranz García <aarranz@conwet.com>
2012-11-16 03:54:32 +0100

    Add copyright header to wirecloudcommons.utils.transaction

--- src/wirecloudcommons/utils/transaction.py
+++ src/wirecloudcommons/utils/transaction.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.db.transaction import is_dirty, leave_transaction_management, rollback, commit, enter_transaction_management, managed
 from django.db import DEFAULT_DB_ALIAS
 from django.http import HttpResponse
