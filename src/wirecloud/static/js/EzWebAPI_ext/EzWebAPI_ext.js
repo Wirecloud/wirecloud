add4e9dca8170962a4840627b2937e3e74e6ef51
Álvaro Arranz García <aarranz@conwet.com>
2012-11-25 00:52:21 +0100

    Make widgets use the StyledButton of the platform

--- src/wirecloud/static/js/EzWebAPI_ext/EzWebAPI_ext.js
+++ src/wirecloud/static/js/EzWebAPI_ext/EzWebAPI_ext.js
@@ -1943,6 +1943,7 @@ var StyledElements = {
     'Event': window.parent.StyledElements.Event,
     'ObjectWithEvents': window.parent.StyledElements.ObjectWithEvents,
     'StyledElement': window.parent.StyledElements.StyledElement,
+    'StyledButton': window.parent.StyledElements.StyledButton,
     'StyledInputElement': window.parent.StyledElements.StyledInputElement,
     'StyledTextField': window.parent.StyledElements.StyledTextField,
     'StyledPasswordField': window.parent.StyledElements.StyledPasswordField,
@@ -3304,64 +3305,6 @@ StyledElements.StyledAlternatives.prototype.showAlternative = function(alternati
         }
     }
     this.transitionsQueue.addCommand(id);
-}
-
-
-/**
- *
- * Eventos que soporta este componente:
- *      - click: evento lanzado cuando se pulsa el botón.
- */
-StyledElements.StyledButton = function(options) {
-    var defaultOptions = {
-        'text': null,
-        'title': '',
-        'class': '',
-        'iconHeight': 24,
-        'iconWidth': 24,
-        'icon': null
-    };
-    options = EzWebExt.merge(defaultOptions, options);
-
-    // Necesario para permitir herencia
-    if (options.extending)
-        return;
-
-    StyledElements.StyledElement.call(this, ['click']);
-
-    this.wrapperElement = document.createElement("div");
-    this.wrapperElement.className = EzWebExt.appendWord(options['class'], "styled_button");
-
-    var button = document.createElement("div");
-    if (options.title)
-        button.setAttribute('title', options.title);
-
-    if (options.icon != null) {
-        this.icon = document.createElement("img");
-        this.icon.className = "icon";
-        this.icon.style.width = options['iconWidth'] + 'px';
-        this.icon.style.height = options['iconHeight'] + 'px';
-        this.icon.src = options.icon;
-        button.appendChild(this.icon);
-    }
-
-    this.wrapperElement.appendChild(button);
-
-    if (options.text !== null) {
-        this.label = document.createElement('span');
-        this.label.appendChild(document.createTextNode(options.text));
-        button.appendChild(this.label);
-    }
-
-    /* Event handlers */
-    EzWebExt.addEventListener(button, 'click', EzWebExt.bind(this._clickCallback, this), true);
-}
-StyledElements.StyledButton.prototype = new StyledElements.StyledElement();
-
-StyledElements.StyledButton.prototype._clickCallback = function(e) {
-    e.stopPropagation();
-    if (this.enabled)
-        this.events['click'].dispatch(this);
 }
 
 /**
