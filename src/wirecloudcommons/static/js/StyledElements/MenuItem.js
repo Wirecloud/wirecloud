8b725880ecba881bad0ea0fab395da7e856129ee
Álvaro Arranz García <aarranz@conwet.com>
2012-12-03 15:03:58 +0100

    Clean up MenuItem.js

--- src/wirecloudcommons/static/js/StyledElements/MenuItem.js
+++ src/wirecloudcommons/static/js/StyledElements/MenuItem.js
@@ -1,57 +1,67 @@
-/**
- *
- */
-StyledElements.MenuItem = function(text, handler, context) {
-    StyledElements.StyledElement.call(this, ['click', 'mouseover', 'mouseout']);
+/*global EzWebExt, StyledElements*/
 
-    this.wrapperElement = document.createElement("div");
-    EzWebExt.addClassName(this.wrapperElement, "menu_item");
+(function () {
 
-    var span = document.createElement("span");
-    span.appendChild(document.createTextNode(text));
-    this.wrapperElement.appendChild(span);
+    "use strict";
 
-    this.run = handler;
-    this.context = context;
+    /**
+     *
+     */
+    var MenuItem = function MenuItem(text, handler, context) {
+        StyledElements.StyledElement.call(this, ['click', 'mouseover', 'mouseout']);
 
-    // Internal events
-    this._mouseoverEventHandler = EzWebExt.bind(function(event) {
-        if (this.enabled) {
-            EzWebExt.addClassName(this.wrapperElement, "hovered");
-            this.events['mouseover'].dispatch(this);
+        this.wrapperElement = document.createElement("div");
+        EzWebExt.addClassName(this.wrapperElement, "menu_item");
+
+        var span = document.createElement("span");
+        span.appendChild(document.createTextNode(text));
+        this.wrapperElement.appendChild(span);
+
+        this.run = handler;
+        this.context = context;
+
+        // Internal events
+        this._mouseoverEventHandler = EzWebExt.bind(function (event) {
+            if (this.enabled) {
+                EzWebExt.addClassName(this.wrapperElement, "hovered");
+                this.events.mouseover.dispatch(this);
+            }
+        }, this);
+        EzWebExt.addEventListener(this.wrapperElement, "mouseover", this._mouseoverEventHandler, false);
+        this._mouseoutEventHandler = EzWebExt.bind(function (event) {
+            if (this.enabled) {
+                EzWebExt.removeClassName(this.wrapperElement, "hovered");
+                this.events.mouseout.dispatch(this);
+            }
+        }, this);
+        EzWebExt.addEventListener(this.wrapperElement, "mouseout", this._mouseoutEventHandler, false);
+
+        this._clickHandler = EzWebExt.bind(function (event) {
+            event.stopPropagation();
+            if (this.enabled) {
+                this.events.mouseout.dispatch(this);
+                this.events.click.dispatch(this);
+            }
+        }, this);
+        EzWebExt.addEventListener(this.wrapperElement, "click", this._clickHandler, false);
+    };
+    MenuItem.prototype = new StyledElements.StyledElement();
+
+    MenuItem.prototype.destroy = function destroy() {
+        if (EzWebExt.XML.isElement(this.wrapperElement.parentNode)) {
+            EzWebExt.removeFromParent(this.wrapperElement);
         }
-    }, this);
-    EzWebExt.addEventListener(this.wrapperElement, "mouseover", this._mouseoverEventHandler, false);
-    this._mouseoutEventHandler = EzWebExt.bind(function(event) {
-        if (this.enabled) {
-            EzWebExt.removeClassName(this.wrapperElement, "hovered");
-            this.events['mouseout'].dispatch(this);
-        }
-    }, this)
-    EzWebExt.addEventListener(this.wrapperElement, "mouseout", this._mouseoutEventHandler, false);
+        EzWebExt.removeEventListener(this.wrapperElement, "mouseover", this._mouseoverEventHandler, false);
+        EzWebExt.removeEventListener(this.wrapperElement, "mouseout", this._mouseoutEventHandler, false);
+        EzWebExt.removeEventListener(this.wrapperElement, "click", this._clickHandler, false);
 
-    this._clickHandler = EzWebExt.bind(function(event) {
-        event.stopPropagation();
-        if (this.enabled) {
-            this.events['mouseout'].dispatch(this);
-            this.events['click'].dispatch(this);
-        }
-    }, this);
-    EzWebExt.addEventListener(this.wrapperElement, "click", this._clickHandler, false);
-}
-StyledElements.MenuItem.prototype = new StyledElements.StyledElement();
+        this._mouseoverEventHandler = null;
+        this._mouseoutEventHandler = null;
+        this._clickHandler = null;
 
-StyledElements.MenuItem.prototype.destroy = function() {
-    if (EzWebExt.XML.isElement(this.wrapperElement.parentNode)) {
-        EzWebExt.removeFromParent(this.wrapperElement);
-    }
-    EzWebExt.removeEventListener(this.wrapperElement, "mouseover", this._mouseoverEventHandler, false);
-    EzWebExt.removeEventListener(this.wrapperElement, "mouseout", this._mouseoutEventHandler, false);
-    EzWebExt.removeEventListener(this.wrapperElement, "click", this._clickHandler, false);
+        StyledElements.StyledElement.prototype.destroy.call(this);
+    };
 
-    this._mouseoverEventHandler = null;
-    this._mouseoutEventHandler = null;
-    this._clickHandler = null;
+    StyledElements.MenuItem = MenuItem;
 
-    StyledElements.StyledElement.prototype.destroy.call(this);
-}
+})();
