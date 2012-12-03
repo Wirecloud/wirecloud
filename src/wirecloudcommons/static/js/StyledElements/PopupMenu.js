9bf4bd9b18317013a64c5ea25c111fe835bc2d8a
Álvaro Arranz García <aarranz@conwet.com>
2012-12-03 15:00:37 +0100

    Clean up PopupMenu.js

--- src/wirecloudcommons/static/js/StyledElements/PopupMenu.js
+++ src/wirecloudcommons/static/js/StyledElements/PopupMenu.js
@@ -1,40 +1,49 @@
-/**
- *
- */
-StyledElements.PopupMenu = function (options) {
-    StyledElements.PopupMenuBase.call(this, options);
+/*global EzWebExt, StyledElements*/
 
-    this._disableCallback = EzWebExt.bind(function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-        this.hide();
-    }, this);
+(function () {
 
-    this._disableLayer = document.createElement('div');
-    this._disableLayer.className = 'disable-layer';
-    EzWebExt.addEventListener(this._disableLayer, "click", this._disableCallback, false);
-    EzWebExt.addEventListener(this._disableLayer, "contextmenu", this._disableCallback, false);
-};
-StyledElements.PopupMenu.prototype = new StyledElements.PopupMenuBase({extending: true});
+    "use strict";
 
-StyledElements.PopupMenu.prototype.show = function(refPosition) {
-    document.body.appendChild(this._disableLayer);
+    /**
+     *
+     */
+    var PopupMenu = function PopupMenu(options) {
+        StyledElements.PopupMenuBase.call(this, options);
 
-    StyledElements.PopupMenuBase.prototype.show.call(this, refPosition);
-}
+        this._disableCallback = EzWebExt.bind(function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+            this.hide();
+        }, this);
 
-StyledElements.PopupMenu.prototype.hide = function() {
-    StyledElements.PopupMenuBase.prototype.hide.call(this);
+        this._disableLayer = document.createElement('div');
+        this._disableLayer.className = 'disable-layer';
+        EzWebExt.addEventListener(this._disableLayer, "click", this._disableCallback, false);
+        EzWebExt.addEventListener(this._disableLayer, "contextmenu", this._disableCallback, false);
+    };
+    PopupMenu.prototype = new StyledElements.PopupMenuBase({extending: true});
 
-    if (EzWebExt.XML.isElement(this._disableLayer.parentNode)) {
-        EzWebExt.removeFromParent(this._disableLayer);
-    }
-}
+    PopupMenu.prototype.show = function show(refPosition) {
+        document.body.appendChild(this._disableLayer);
 
-StyledElements.PopupMenu.prototype.destroy = function() {
-    EzWebExt.removeEventListener(this._disableLayer, "click", this._disableCallback, false);
-    EzWebExt.removeEventListener(this._disableLayer, "contextmenu", this._disableCallback, false);
-    this._disableCallback = null;
+        StyledElements.PopupMenuBase.prototype.show.call(this, refPosition);
+    };
 
-    StyledElements.PopupMenuBase.prototype.destroy.call(this);
-}
+    PopupMenu.prototype.hide = function hide() {
+        StyledElements.PopupMenuBase.prototype.hide.call(this);
+
+        if (EzWebExt.XML.isElement(this._disableLayer.parentNode)) {
+            EzWebExt.removeFromParent(this._disableLayer);
+        }
+    };
+
+    PopupMenu.prototype.destroy = function destroy() {
+        EzWebExt.removeEventListener(this._disableLayer, "click", this._disableCallback, false);
+        EzWebExt.removeEventListener(this._disableLayer, "contextmenu", this._disableCallback, false);
+        this._disableCallback = null;
+
+        StyledElements.PopupMenuBase.prototype.destroy.call(this);
+    };
+
+    StyledElements.PopupMenu = PopupMenu;
+})();
