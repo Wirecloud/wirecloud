b6d58c00cc5d226c88ed0a29d723e815ac21e88e
Álvaro Arranz <aarranz@conwet.com>
2013-02-04 11:51:40 +0100

    Add wirecloud.commons.utils.endocing module

---
+++ src/wirecloud/commons/utils/encoding.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2013 Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.utils.functional import Promise
+from django.utils.translation import force_unicode
+from django.utils.simplejson import JSONEncoder
+
+class LazyEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Promise):
+            return force_unicode(o)
+        else:
+            return super(LazyEncoder, self).default(o)
