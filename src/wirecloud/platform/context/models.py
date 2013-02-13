e033fa49673cdc1b682edcc2aaf1e140a73ab1b4
Álvaro Arranz <aarranz@conwet.com>
2013-02-13 16:56:56 +0100

    Remove scope attribute from Constant

--- src/wirecloud/platform/context/models.py
+++ src/wirecloud/platform/context/models.py
@@ -24,12 +24,10 @@ from django.utils.translation import gettext_lazy as _
 
 class Constant(models.Model):
 
-    scope = models.CharField(_('Scope'), max_length=20, null=False, blank=False)
-    concept = models.CharField(_('Concept'), max_length=255, null=False, blank=False)
+    concept = models.CharField(_('Concept'), max_length=255, unique=True, null=False, blank=False)
     value = models.CharField(_('Value'), max_length=256)
 
     class Meta:
-        unique_together = (('scope', 'concept'),)
         app_label = 'platform'
         db_table = 'wirecloud_constant'
 
