9a463e1b8cd5da51c3335cef19e2e42be14bcddb
Carlos Blanco <cblanco@conwet.com>
2013-02-22 13:34:19 +0100

    Little change. setTextContent by .textContent in WiringEditor.js

--- src/wirecloud/platform/static/js/wirecloud/ui/WiringEditor.js
+++ src/wirecloud/platform/static/js/wirecloud/ui/WiringEditor.js
@@ -66,7 +66,7 @@ if (!Wirecloud.ui) {
         this.emptyBox.classList.add('alert-block');
         // Title
         var pTitle = document.createElement('h4');
-        pTitle.setTextContent(gettext("Welcome to the Wiring Editor view!"));
+        pTitle.textContent = gettext("Welcome to the Wiring Editor view!");
         this.emptyBox.appendChild(pTitle);
         // Message
         var message = document.createElement('p');
