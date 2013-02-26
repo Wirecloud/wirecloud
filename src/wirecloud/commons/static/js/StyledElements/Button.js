fbaa1066ec0cf931a8928b8760ac3285aac8f204
Álvaro Arranz <aarranz@conwet.com>
2013-02-26 17:34:13 +0100

    Allow to click buttons using the enter key

--- src/wirecloud/commons/static/js/StyledElements/Button.js
+++ src/wirecloud/commons/static/js/StyledElements/Button.js
@@ -4,6 +4,20 @@
 
     "use strict";
 
+    var clickCallback = function clickCallback(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (this.enabled) {
+            this.events.click.dispatch(this);
+        }
+    };
+
+    var keydownCallback = function keydownCallback(e) {
+        if (this.enabled && e.keyCode === 13) {
+            this.events.click.dispatch(this);
+        }
+    };
+
     /**
      *
      * Eventos que soporta este componente:
@@ -43,6 +57,7 @@
             button = document.createElement("div");
             this.wrapperElement.appendChild(button);
         }
+        this._button = button;
         button.setAttribute('tabindex', '0');
 
         if (options.title) {
@@ -66,19 +81,16 @@
         }
 
         /* Event handlers */
+        this._clickCallback = clickCallback.bind(this);
+        this._keydownCallback = keydownCallback.bind(this);
+
         EzWebExt.addEventListener(button, 'mousedown', EzWebExt.stopPropagationListener, true);
-        EzWebExt.addEventListener(button, 'click', EzWebExt.bind(this._clickCallback, this), true);
+        EzWebExt.addEventListener(button, 'click', this._clickCallback, true);
+        button.addEventListener('keydown', this._keydownCallback, true);
+
         this.buttonElement = button;
     };
     StyledButton.prototype = new StyledElements.StyledElement();
-
-    StyledButton.prototype._clickCallback = function _clickCallback(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        if (this.enabled) {
-            this.events.click.dispatch(this);
-        }
-    };
 
     StyledButton.prototype.focus = function focus() {
         this.buttonElement.focus();
@@ -90,6 +102,19 @@
 
     StyledButton.prototype.setTitle = function setTitle(title) {
         this.buttonElement.setAttribute('title', title);
+    };
+
+    StyledButton.prototype.destroy = function destroy() {
+
+        this._button.removeEventListener('mousedown', EzWebExt.stopPropagationListener, true);
+        this._button.removeEventListener('click', this._clickCallback, true);
+        this._button.removeEventListener('keydown', this._keydownCallback, true);
+
+        delete this._button;
+        delete this._clickCallback;
+        delete this._keydownCallback;
+
+        StyledElements.StyledElement.prototype.destroy.call(this);
     };
 
     StyledElements.StyledButton = StyledButton;
