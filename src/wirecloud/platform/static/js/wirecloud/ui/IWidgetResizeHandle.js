04c3e00a24064b704cbab1fe0134cbd841f89420
Álvaro Arranz <aarranz@conwet.com>
2013-02-28 18:11:35 +0100

    Rework IWidgetResizeHandle to follow Wirecloud's code style

--- src/wirecloud/platform/static/js/wirecloud/ui/IWidgetResizeHandle.js
+++ src/wirecloud/platform/static/js/wirecloud/ui/IWidgetResizeHandle.js
@@ -1,73 +1,83 @@
-var IWidgetResizeHandle = function IWidgetResizeHandle(iWidget, resizeLeftSide) {
-    StyledElements.StyledElement.call(this, []);
+/*global ResizeHandle, StyledElements*/
 
-    this.wrapperElement = document.createElement('div');
+(function () {
 
-    ResizeHandle.call(this, iWidget.element, this.wrapperElement,
-                            {iWidget: iWidget, resizeLeftSide: resizeLeftSide},
-                            IWidgetResizeHandle.prototype.startFunc,
-                            IWidgetResizeHandle.prototype.updateFunc,
-                            IWidgetResizeHandle.prototype.finishFunc,
-                            IWidgetResizeHandle.prototype.canBeResizedFunc);
-};
-IWidgetResizeHandle.prototype = new StyledElements.StyledElement();
+    "use strict";
 
-IWidgetResizeHandle.prototype.canBeResizedFunc = function canBeResizedFunc(resizableElement, data) {
-    return data.iWidget.isAllowed('resize');
-};
+    var IWidgetResizeHandle = function IWidgetResizeHandle(iWidget, resizeLeftSide) {
+        StyledElements.StyledElement.call(this, []);
 
-IWidgetResizeHandle.prototype.startFunc = function startFunc(resizableElement, handleElement, data) {
-    handleElement.addClassName("inUse");
-    // TODO merge with iwidget minimum sizes
-    data.minWidth = Math.ceil(data.iWidget.layout.fromPixelsToHCells(80));
-    data.minHeight = Math.ceil(data.iWidget.layout.fromPixelsToVCells(50));
-    data.innitialWidth = data.iWidget.getWidth();
-    data.innitialHeight = data.iWidget.getHeight();
-    data.oldZIndex = data.iWidget.getZPosition();
-    data.iWidget.setZPosition("999999");
-    data.dragboard = data.iWidget.layout.dragboard;
-};
+        this.wrapperElement = document.createElement('div');
 
-IWidgetResizeHandle.prototype.updateFunc = function updateFunc(resizableElement, handleElement, data, x, y) {
-    var iWidget = data.iWidget;
+        ResizeHandle.call(this, iWidget.element, this.wrapperElement,
+                                {iWidget: iWidget, resizeLeftSide: resizeLeftSide},
+                                IWidgetResizeHandle.prototype.startFunc,
+                                IWidgetResizeHandle.prototype.updateFunc,
+                                IWidgetResizeHandle.prototype.finishFunc,
+                                IWidgetResizeHandle.prototype.canBeResizedFunc);
+    };
+    IWidgetResizeHandle.prototype = new StyledElements.StyledElement();
 
-    // Skip if the mouse is outside the dragboard
-    if (iWidget.layout.isInside(x, y)) {
-        var position = iWidget.layout.getCellAt(x, y);
-        var currentPosition = iWidget.getPosition();
-        var width;
+    IWidgetResizeHandle.prototype.canBeResizedFunc = function canBeResizedFunc(resizableElement, data) {
+        return data.iWidget.isAllowed('resize');
+    };
 
-        if (data.resizeLeftSide) {
-            width = currentPosition.x + iWidget.getWidth() - position.x;
-        } else {
-            width = position.x - currentPosition.x + 1;
+    IWidgetResizeHandle.prototype.startFunc = function startFunc(resizableElement, handleElement, data) {
+        handleElement.addClassName("inUse");
+        // TODO merge with iwidget minimum sizes
+        data.minWidth = Math.ceil(data.iWidget.layout.fromPixelsToHCells(80));
+        data.minHeight = Math.ceil(data.iWidget.layout.fromPixelsToVCells(50));
+        data.innitialWidth = data.iWidget.getWidth();
+        data.innitialHeight = data.iWidget.getHeight();
+        data.oldZIndex = data.iWidget.getZPosition();
+        data.iWidget.setZPosition("999999");
+        data.dragboard = data.iWidget.layout.dragboard;
+    };
+
+    IWidgetResizeHandle.prototype.updateFunc = function updateFunc(resizableElement, handleElement, data, x, y) {
+        var iWidget = data.iWidget;
+
+        // Skip if the mouse is outside the dragboard
+        if (iWidget.layout.isInside(x, y)) {
+            var position = iWidget.layout.getCellAt(x, y);
+            var currentPosition = iWidget.getPosition();
+            var width;
+
+            if (data.resizeLeftSide) {
+                width = currentPosition.x + iWidget.getWidth() - position.x;
+            } else {
+                width = position.x - currentPosition.x + 1;
+            }
+            var height = position.y - currentPosition.y + 1;
+
+            // Minimum width
+            if (width < data.minWidth) {
+                width = data.minWidth;
+            }
+
+            // Minimum height
+            if (height < data.minHeight) {
+                height = data.minHeight;
+            }
+
+            if (width !== iWidget.getWidth() || height !== iWidget.getHeight()) {
+                iWidget.setSize(width, height, data.resizeLeftSide, false);
+            }
         }
-        var height = position.y - currentPosition.y + 1;
+    };
 
-        // Minimum width
-        if (width < data.minWidth) {
-            width = data.minWidth;
+    IWidgetResizeHandle.prototype.finishFunc = function finishFunc(resizableElement, handleElement, data) {
+        var iWidget = data.iWidget;
+        data.iWidget.setZPosition(data.oldZIndex);
+        if (data.innitialWidth !== data.iWidget.getWidth() || data.innitialHeight !== data.iWidget.getHeight()) {
+            iWidget.setSize(iWidget.getWidth(), iWidget.getHeight(), data.resizeLeftSide, true);
         }
+        handleElement.removeClassName("inUse");
 
-        // Minimum height
-        if (height < data.minHeight) {
-            height = data.minHeight;
-        }
+        // This is needed to check if the scrollbar status has changed (visible/hidden)
+        data.dragboard._notifyWindowResizeEvent();
+    };
 
-        if (width !== iWidget.getWidth() || height !== iWidget.getHeight()) {
-            iWidget.setSize(width, height, data.resizeLeftSide, false);
-        }
-    }
-};
+    window.IWidgetResizeHandle = IWidgetResizeHandle;
 
-IWidgetResizeHandle.prototype.finishFunc = function finishFunc(resizableElement, handleElement, data) {
-    var iWidget = data.iWidget;
-    data.iWidget.setZPosition(data.oldZIndex);
-    if (data.innitialWidth !== data.iWidget.getWidth() || data.innitialHeight !== data.iWidget.getHeight()) {
-        iWidget.setSize(iWidget.getWidth(), iWidget.getHeight(), data.resizeLeftSide, true);
-    }
-    handleElement.removeClassName("inUse");
-
-    // This is needed to check if the scrollbar status has changed (visible/hidden)
-    data.dragboard._notifyWindowResizeEvent();
-};
+})();
