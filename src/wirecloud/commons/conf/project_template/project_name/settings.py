e80b5a9bee2bab7cb3a70bcd4c640a4ac8137101
Álvaro Arranz <aarranz@conwet.com>
2013-03-01 01:12:54 +0100

    Fix problem in settings.py template

--- src/wirecloud/commons/conf/project_template/project_name/settings.py
+++ src/wirecloud/commons/conf/project_template/project_name/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASEDIR, '../static')
+STATIC_ROOT = path.join(BASEDIR, '../static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
