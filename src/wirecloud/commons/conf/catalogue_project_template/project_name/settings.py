7547678c5b8dfe3194f4a50f426d35bb02bedfc6
Álvaro Arranz <aarranz@conwet.com>
2013-06-20 17:01:16 +0200

    Add an initial implementation of a quick-start option

--- src/wirecloud/commons/conf/catalogue_project_template/project_name/settings.py
+++ src/wirecloud/commons/conf/catalogue_project_template/project_name/settings.py
@@ -20,8 +20,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': {{ db_engine }},       # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': {{ db_name }},           # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
