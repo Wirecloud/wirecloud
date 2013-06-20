a7bdefd826615e7b2e231ecd76d965395cc89487
Álvaro Arranz <aarranz@conwet.com>
2013-06-20 17:14:25 +0200

    Add ALLOWED_HOSTS for compability with Django 1.5+

--- src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
+++ src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
@@ -30,6 +30,11 @@ DATABASES = {
     }
 }
 
+# This setting has only effect in DJango 1.5+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
