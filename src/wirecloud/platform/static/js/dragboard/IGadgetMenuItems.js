f97ae0f19c49f294bef84ab9afccdb8c557af61a
Álvaro Arranz <aarranz@conwet.com>
2013-06-21 16:36:01 +0200

    Fix some copyright headers

--- src/wirecloud/platform/static/js/dragboard/IGadgetMenuItems.js
+++ src/wirecloud/platform/static/js/dragboard/IGadgetMenuItems.js
@@ -1,5 +1,5 @@
 /*
- *     (C) Copyright 2012-2013 Universidad Politécnica de Madrid
+ *     Copyright (c) 2012-2013 CoNWeT Lab., Universidad Politécnica de Madrid
  *
  *     This file is part of Wirecloud Platform.
  *
