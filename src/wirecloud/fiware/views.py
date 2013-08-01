06c3a417f0270d76a7fcc9e94fdb40f9952b9d12
Álvaro Arranz <aarranz@conwet.com>
2013-08-01 13:58:55 +0200

    Add a login view that automatically starts the oauth2 flow for authenticating using the IdM server

---
+++ src/wirecloud/fiware/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012-2013 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+
+def login(request):
+
+    if request.user.is_authenticated():
+        url = request.GET.get(REDIRECT_FIELD_NAME, '/')
+    else:
+        url = reverse('socialauth_begin', kwargs={'backend': 'fiware'}) + '?' + request.GET.urlencode() 
+
+    return HttpResponseRedirect(url)
