868fd2d4d3674cc75ef27b3f8cbcbbd8c8425f3f
Álvaro Arranz <aarranz@conwet.com>
2013-08-28 23:52:32 +0200

    Initial template handling tests

---
+++ src/wirecloud/commons/tests.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012-2013 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.utils.unittest import TestCase
+
+from wirecloud.commons.utils.template.parsers import TemplateParser
+from wirecloud.commons.utils.template.writers.rdf import write_rdf_description
+from wirecloud.commons.utils.template.writers.xml import write_xml_description
+
+
+class TemplateUtilsTestCase(TestCase):
+
+    tags = ('template',)
+    maxDiff = None
+
+    @classmethod
+    def setUpClass(cls):
+
+        super(TemplateUtilsTestCase, cls).setUpClass()
+
+        cls.basic_widget_info = {
+            'type': 'widget',
+            'vendor': 'Wirecloud',
+            'name': 'TemplateTest',
+            'version': '1.0',
+            'display_name': 'TemplateTest',
+            'description': '',
+            'author': '',
+            'email': 'email@example.com',
+            'image_uri': '',
+            'iphone_image_uri': '',
+            'doc_uri': '',
+            'requirements': [],
+            'preferences': [],
+            'properties': [],
+            'context': [],
+            'wiring': {
+                'inputs': [],
+                'outputs': [],
+            },
+            'code_url': 'http://example.com/code.html',
+            'code_content_type': 'application/xhtml+xml',
+            'code_cacheable': True,
+            'code_uses_platform_style': False,
+            'default_lang': 'en',
+            'widget_width': '8',
+            'widget_height': '30',
+            'translations': {},
+            'translation_index_usage': {},
+        }
+
+        cls.widget_info = {
+            'type': 'widget',
+            'vendor': 'Wirecloud',
+            'name': 'TemplateTest',
+            'version': '2.0',
+            'display_name': 'Template Test',
+            'description': 'test',
+            'author': 'author_test',
+            'email': 'test@example.com',
+            'image_uri': 'images/catalogue.png',
+            'iphone_image_uri': 'images/smartphone.png',
+            'doc_uri': 'docs/index.html',
+            'requirements': [
+                {'type': 'feature', 'name': 'Wirecloud'},
+            ],
+            'preferences': [
+            ],
+            'properties': [
+            ],
+            'context': [],
+            'wiring': {
+                'inputs': [
+                ],
+                'outputs': [
+                ],
+            },
+            'code_url': 'http://example.com/code.html',
+            'code_content_type': 'application/xhtml+xml',
+            'code_cacheable': False,
+            'code_uses_platform_style': True,
+            'default_lang': 'en',
+            'widget_width': '8',
+            'widget_height': '30',
+            'translations': {},
+            'translation_index_usage': {},
+        }
+
+    def test_rdf_parser_writer_basic_widget(self):
+
+        rdf_description = write_rdf_description(self.basic_widget_info)
+        template = TemplateParser(rdf_description)
+        processed_info = template.get_resource_info()
+
+        self.assertEqual(processed_info, self.basic_widget_info)
+
+    def test_rdf_parser_writer_widget(self):
+
+        rdf_description = write_rdf_description(self.widget_info)
+        template = TemplateParser(rdf_description)
+        processed_info = template.get_resource_info()
+
+        self.assertEqual(processed_info, self.widget_info)
+
+    def test_xml_parser_writer_basic_widget(self):
+
+        xml_description = write_xml_description(self.basic_widget_info)
+        template = TemplateParser(xml_description)
+        processed_info = template.get_resource_info()
+
+        self.assertEqual(processed_info, self.basic_widget_info)
+
+    def test_xml_parser_writer_widget(self):
+
+        xml_description = write_xml_description(self.widget_info)
+        template = TemplateParser(xml_description)
+        processed_info = template.get_resource_info()
+
+        self.assertEqual(processed_info, self.widget_info)
