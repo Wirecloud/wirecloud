3c1714ad7629a7f12bb2e887f9cdf91868d0d1ba
Carlos Blanco <cblanco@conwet.com>
2013-11-06 16:39:22 +0100

    License Change in Wiring Editor.
    Now it's licensed under Apache License Version 2.0 and distributed in
    Wircloud using Wirecloud's License.
    
    Signed-off-by: Álvaro Arranz <aarranz@conwet.com>

---
+++ THIRD_PARTY.md
@@ -0,0 +1,8 @@
+The software includes the following third-party libraries:
+
+- WiringEditor: Center Open Middleware Wiring Editor for javascript, licensed under:
+
+  + The Apache Software License, Version 2.0
+
+Copies of the abovementioned licenses can be found in the *licenses* directory of
+this distribution.
--- src/wirecloud/platform/static/js/wirecloud/ui/WiringEditor/Arrow.js
+++ src/wirecloud/platform/static/js/wirecloud/ui/WiringEditor/Arrow.js
@@ -1,23 +1,21 @@
 /*
- *     Copyright (c) 2012-2013 CoNWeT Lab., Universidad Politécnica de Madrid
- *     Copyright (c) 2012-2013 Center for Open Middleware
+ *     DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
- *     This file is part of Wirecloud Platform.
+ *     Copyright (c) 2012-2013 Universidad Politécnica de Madrid
+ *     Copyright (c) 2012-2013 the Center for Open Middleware
  *
- *     Wirecloud Platform is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU Affero General Public License as
- *     published by the Free Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
+ *     Licensed under the Apache License, Version 2.0 (the
+ *     "License"); you may not use this file except in compliance
+ *     with the License.  You may obtain a copy of the License at
  *
- *     Wirecloud is distributed in the hope that it will be useful, but WITHOUT
- *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
- *     License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with Wirecloud Platform.  If not, see
- *     <http://www.gnu.org/licenses/>.
- *
+ *     Unless required by applicable law or agreed to in writing,
+ *     software distributed under the License is distributed on an
+ *     "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *     KIND, either express or implied.  See the License for the
+ *     specific language governing permissions and limitations
+ *     under the License.
  */
 
 /*global Draggable, EzWebExt, Wirecloud, Event, EzWebEffectBase, Element */
