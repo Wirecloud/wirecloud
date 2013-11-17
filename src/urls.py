5a84249b7e96d9d2f82ee1b27a33b7978d63b16e
Álvaro Arranz <aarranz@conwet.com>
2013-11-17 23:49:09 +0100

    Remove django < 1.4 code

--- src/urls.py
+++ src/urls.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 # urls.py used as base for developing wirecloud.
 
-try:
-    from django.conf.urls import patterns, include, url
-except ImportError:  # pragma: no cover
-    # for Django version less than 1.4
-    from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
