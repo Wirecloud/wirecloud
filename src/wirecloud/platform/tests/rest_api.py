2c9d09da7f7d8f51f6573b58740ee7b8b75fb5fe
Álvaro Arranz <aarranz@conwet.com>
2013-12-16 21:22:55 +0100

    Fix some copyright headers

--- src/wirecloud/platform/tests/rest_api.py
+++ src/wirecloud/platform/tests/rest_api.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import json
 import os
 import shutil
