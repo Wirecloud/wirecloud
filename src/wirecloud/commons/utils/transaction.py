1a7dec871bc18fd9a9e4228316c2b60c5efa1d69
Álvaro Arranz <aarranz@conwet.com>
2014-01-02 19:45:54 +0100

    Make commit_on_http_success django 1.6 compatible

--- src/wirecloud/commons/utils/transaction.py
+++ src/wirecloud/commons/utils/transaction.py
@@ -21,40 +21,58 @@ from django.db.transaction import is_dirty, leave_transaction_management, rollba
 from django.db import DEFAULT_DB_ALIAS
 from django.http import HttpResponse
 
+try:
+    from django.db.transaction import atomic, set_rollback
 
-def commit_on_http_success(func, using=None):
-    """
-    This decorator activates db commit on HTTP success response. This way, if the
-    view function return a success reponse, a commit is made; if the viewfunc
-    produces an exception or return an error response, a rollback is made.
-    """
-    if using is None:
-        using = DEFAULT_DB_ALIAS
+    def commit_on_http_success(func, using=None):
 
-    def wrapped_func(*args, **kwargs):
-        enter_transaction_management(using=using)
-        managed(True, using=using)
+        def wrapped_func(*args, **kwargs):
 
-        try:
-            res = func(*args, **kwargs)
-        except:
-            if is_dirty(using=using):
-                rollback(using=using)
-            raise
-        else:
-            if is_dirty(using=using):
+            with atomic(using=using):
+                res = func(*args, **kwargs)
 
                 if not isinstance(res, HttpResponse) or res.status_code < 200 or res.status_code >= 400:
+                    set_rollback(True, using=using)
+
+            return res
+
+        return wrapped_func
+
+except:
+
+    def commit_on_http_success(func, using=None):
+        """
+        This decorator activates db commit on HTTP success response. This way, if the
+        view function return a success reponse, a commit is made; if the viewfunc
+        produces an exception or return an error response, a rollback is made.
+        """
+        if using is None:
+            using = DEFAULT_DB_ALIAS
+
+        def wrapped_func(*args, **kwargs):
+            enter_transaction_management(using=using)
+            managed(True, using=using)
+
+            try:
+                res = func(*args, **kwargs)
+            except:
+                if is_dirty(using=using):
                     rollback(using=using)
-                else:
-                    try:
-                        commit(using=using)
-                    except:
+                raise
+            else:
+                if is_dirty(using=using):
+
+                    if not isinstance(res, HttpResponse) or res.status_code < 200 or res.status_code >= 400:
                         rollback(using=using)
-                        raise
-        finally:
-            leave_transaction_management(using=using)
+                    else:
+                        try:
+                            commit(using=using)
+                        except:
+                            rollback(using=using)
+                            raise
+            finally:
+                leave_transaction_management(using=using)
 
-        return res
+            return res
 
-    return wrapped_func
+        return wrapped_func
