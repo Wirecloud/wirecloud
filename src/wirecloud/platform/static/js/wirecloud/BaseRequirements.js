586644006297b3e74ac8fd6d3d60704a94986f06
Álvaro Arranz <aarranz@conwet.com>
2014-01-14 17:11:36 +0100

    Add json support check to BaseRequirements

--- src/wirecloud/platform/static/js/wirecloud/BaseRequirements.js
+++ src/wirecloud/platform/static/js/wirecloud/BaseRequirements.js
@@ -28,6 +28,10 @@
     var check_basic_requirements = function check_basic_requirements() {
         var tester = {};
 
+        if (!('JSON' in window)) {
+            throw new Error('Missing JSON support');
+        }
+
         if (!('freeze' in Object)) {
             throw new Error('Missing Object.freeze support');
         }
