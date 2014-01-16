485497e65f32d38d0baa639c2e42d2b432c11101
Álvaro Arranz <aarranz@conwet.com>
2014-01-16 06:18:36 +0100

    Fix regression

--- src/wirecloud/catalogue/models.py
+++ src/wirecloud/catalogue/models.py
@@ -92,7 +92,7 @@ class CatalogueResource(TransModel):
     def get_template(self, request=None):
 
         if urlparse(self.template_uri).scheme == '':
-            template_uri = get_absolute_reverse_url('wirecloud_showcase.media', kwargs={
+            template_uri = get_absolute_reverse_url('wirecloud.showcase_media', kwargs={
                 'vendor': self.vendor,
                 'name': self.short_name,
                 'version': self.version,
