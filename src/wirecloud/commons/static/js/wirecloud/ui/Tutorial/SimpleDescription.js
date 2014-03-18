c75ba961499c4de23cc079cae596a390f2b68898
Álvaro Arranz <aarranz@conwet.com>
2014-03-18 23:52:24 +0100

    Fix SimpleDescription placement

--- src/wirecloud/commons/static/js/wirecloud/ui/Tutorial/SimpleDescription.js
+++ src/wirecloud/commons/static/js/wirecloud/ui/Tutorial/SimpleDescription.js
@@ -1,5 +1,5 @@
 /*
- *     (C) Copyright 2012 Universidad Politécnica de Madrid
+ *     Copyright 2013-2014 CoNWeT Lab., Universidad Politécnica de Madrid
  *
  *     This file is part of Wirecloud Platform.
  *
@@ -24,6 +24,48 @@
 (function () {
 
     "use strict";
+
+    var setDialogPosition = function setDialogPosition(ref_pos, pos) {
+        switch (pos) {
+            case 'up':
+                this.htmlElement.style.top = (ref_pos.top - this.htmlElement.offsetHeight - 20) + 'px';
+                this.htmlElement.style.left = (ref_pos.left + (ref_pos.width - this.htmlElement.offsetWidth) / 2) + 'px';
+                break;
+            case 'right':
+                this.htmlElement.style.top = (ref_pos.top + (ref_pos.height - this.htmlElement.offsetHeight) / 2) + 'px';
+                this.htmlElement.style.left = (ref_pos.right + 20) + 'px';
+                break;
+            case 'left':
+                this.htmlElement.style.top = (ref_pos.top + (ref_pos.height - this.htmlElement.offsetHeight) / 2) + 'px';
+                this.htmlElement.style.left = (ref_pos.left - this.htmlElement.offsetWidth - 20) + 'px';
+                break;
+            case 'down':
+                this.htmlElement.style.top = (ref_pos.bottom + 20) + 'px';
+                this.htmlElement.style.left = (ref_pos.left + (ref_pos.width - this.htmlElement.offsetWidth) / 2) + 'px';
+                break;
+        }
+    };
+
+    var standsOut = function standsOut() {
+        var parent_box = document.body.getBoundingClientRect();
+        var element_box = this.htmlElement.getBoundingClientRect();
+
+        var visible_width = element_box.width - Math.max(element_box.right - parent_box.right, 0) - Math.max(parent_box.left - element_box.left, 0);
+        var visible_height = element_box.height - Math.max(element_box.bottom - parent_box.bottom, 0) - Math.max(parent_box.top - element_box.top, 0);
+        var element_area = element_box.width * element_box.height;
+        var visible_area = visible_width * visible_height;
+        return element_area - visible_area;
+    };
+
+    var fixPosition = function fixPosition(refPosition, weights, positions) {
+        var best_weight = Math.min.apply(Math, weights);
+        var index = weights.indexOf(best_weight);
+        var position = positions[index];
+
+        setDialogPosition.call(this, refPosition, position);
+
+        // TODO
+    };
 
     /**
      * Simple Description Constructor
@@ -116,7 +158,7 @@
     SimpleDescription.prototype.setNext = function setNext() {
         this.nextButton.addEventListener('click', function () {
             if (this.element != null) {
-                this.element.removeClassName('tuto_highlight');
+                this.element.classList.remove('tuto_highlight');
             }
             this.tutorial.nextStep();
         }.bind(this));
@@ -126,6 +168,8 @@
      * activate this step
      */
     var _activate = function _activate() {
+        var i = 0, ref_pos, weights, positions;
+
         this.htmlElement.classList.add("activeStep");
         if (typeof this.element === 'function') {
             this.element = this.element();
@@ -135,22 +179,19 @@
             this.tutorial.resetControlLayer(false);
         }
         if (this.element != null) {
-            var pos = this.element.getBoundingClientRect();
-            switch (this.pos) {
-                case 'up':
-                    this.htmlElement.style.top = (pos.top - this.htmlElement.offsetHeight - 20) + 'px';
-                    break;
-                case 'right':
-                    this.htmlElement.style.left = (pos.right + 20) + 'px';
-                    break;
-                case 'left':
-                    this.htmlElement.style.left = (pos.left - this.htmlElement.offsetWidth - 20) + 'px';
-                    break;
-                default:
-                case 'down':
-                    this.htmlElement.style.top = (pos.bottom + 20) + 'px';
-                    break;
+            ref_pos = this.element.getBoundingClientRect();
+            weights = [];
+            positions = ['down', 'left', 'top', 'right'];
+            do {
+                setDialogPosition.call(this, ref_pos, positions[i]);
+                weights.push(standsOut.call(this));
+                i += 1;
+            } while (weights[i - 1] > 0 && i < positions.length);
+
+            if (weights[i - 1] > 0) {
+                fixPosition.call(this, ref_pos, weights, positions);
             }
+
         } else {
             this.htmlElement.style.top = ((window.innerHeight / 2) - (this.htmlElement.offsetHeight / 2)) + 'px';
             this.htmlElement.style.left = ((window.innerWidth / 2) - (this.htmlElement.offsetWidth / 2)) + 'px';
