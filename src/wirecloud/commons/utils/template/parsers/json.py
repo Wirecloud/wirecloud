a90384846f64331cff53b8d8a7165ce26b13f5d1
Álvaro Arranz <aarranz@conwet.com>
2014-04-24 22:11:06 +0200

    Use unicode_literals in the json parser/writer

--- src/wirecloud/commons/utils/template/parsers/json.py
+++ src/wirecloud/commons/utils/template/parsers/json.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
 import json
 
