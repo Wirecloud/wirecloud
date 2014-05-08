931d467c3b2cd1d274153aa90480e3568d0996c7
Álvaro Arranz <aarranz@conwet.com>
2014-05-08 11:39:08 +0200

    Deprecate wirecloud.commmons.utils.urlresolvers

--- src/wirecloud/commons/utils/urlresolvers.py
+++ src/wirecloud/commons/utils/urlresolvers.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+
 from django.core.urlresolvers import reverse
 from django.utils.functional import lazy
+
+
+warnings.warn('wirecloud.commmons.utils.urlresolvers has been deprecated. Please, use django.core.urlresolvers.reverse_lazy directly.', DeprecationWarning)
 
 reverse_lazy = lazy(reverse, str)
