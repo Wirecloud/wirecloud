0f9e348e858de2e37e33fa78d2d4c9f6768624dd
Álvaro Arranz <aarranz@conwet.com>
2014-06-06 19:32:45 +0200

    New catalogue command: resetsearchindexes

---
+++ src/wirecloud/catalogue/management/commands/resetsearchindexes.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012-2014 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import shutil
+from optparse import make_option
+
+from django.core.management.base import CommandError, NoArgsCommand
+from django.utils.six.moves import input
+
+from wirecloud.catalogue.models import add_document, CatalogueResource
+
+
+class Command(NoArgsCommand):
+    help = 'Resets search indexes'
+    option_list = NoArgsCommand.option_list + (
+        make_option('--noinput',
+            action='store_false', dest='interactive', default=True,
+            help="Do NOT prompt the user for input of any kind."),
+    )
+
+    def handle_noargs(self, **options):
+
+        self.interactive = options['interactive']
+        self.verbosity = int(options.get('verbosity', 1))
+
+        from django.conf import settings
+
+        dirname = settings.WIRECLOUD_INDEX_DIR
+
+        message = ['\n']
+        message.append(
+            'You have requested to reset indexes found in the location\n'
+            'specified in your settings:\n\n'
+            '    %s\n\n' % dirname
+        )
+        message.append('This will DELETE EXISTING FILES!\n')
+        message.append(
+            'Are you sure you want to do this?\n\n'
+            "Type 'yes' to continue, or 'no' to cancel: "
+        )
+
+        if self.interactive and input(''.join(message)) != 'yes':
+            raise CommandError("Reset search indexes cancelled.")
+
+        shutil.rmtree(dirname)
+
+        for resource in CatalogueResource.objects.all():
+            self.log('Adding %s' % resource.local_uri_part)
+            add_document(CatalogueResource, resource, False, False)
+
+    def log(self, msg, level=2):
+        """
+        Small log helper
+        """
+        if self.verbosity >= level:
+            self.stdout.write(msg)
