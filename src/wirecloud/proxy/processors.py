6f92cd5a34864c7cb70624d65e2ba07ab754eeb7
Álvaro Arranz <aarranz@conwet.com>
2014-06-24 07:44:29 +0200

    Convert variable value to bytes before replacing request body in the secure process code

--- src/wirecloud/proxy/processors.py
+++ src/wirecloud/proxy/processors.py
@@ -109,7 +109,7 @@ def process_secure_data(text, request, ignore_errors=False):
                 elif encoding == 'base64':
                     value = value.encode('base64')[:-1]
 
-                request['data'] = BytesIO(request['data'].read().replace(substr, value))
+                request['data'] = BytesIO(request['data'].read().replace(substr, bytes(value)))
 
             elif action == 'basic_auth':
                 user_ref = options.get('user_ref', '')
