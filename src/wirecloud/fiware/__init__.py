39e2919b18c630eed890186a8da88e819e3ac34c
Álvaro Arranz <aarranz@conwet.com>
2014-08-04 11:14:00 +0200

    Update fiware info

--- src/wirecloud/fiware/__init__.py
+++ src/wirecloud/fiware/__init__.py
@@ -17,11 +17,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
-__version_info__ = (3, 3)
+__version_info__ = (3, 4)
 __version__ = '.'.join(map(str, __version_info__))
 
 DEFAULT_FIWARE_PORTALS = (
-    {'name': 'Cloud', 'url': 'http://cloud.lab.fi-ware.org', "logout_path": "/logout"},
+    {'name': 'Cloud', 'url': 'https://cloud.lab.fi-ware.org', "logout_path": "/logout"},
     {'name': 'Store', 'url': 'https://store.lab.fi-ware.org', "logout_path": "/logout"},
     {'name': 'Mashup', 'url': 'https://mashup.lab.fi-ware.org', "logout_path": "/logout", "active": True},
     {'name': 'Account', 'url': 'https://account.lab.fi-ware.org', "logout_path": "/users/sign_out"},
