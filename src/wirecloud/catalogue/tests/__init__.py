545b390859127ee622b7d00a078bf75ad916404f
Álvaro Arranz <aarranz@fi.upm.es>
2015-03-12 23:30:00 +0100

    Add tests for the update_resource_catalogue_cache function

--- src/wirecloud/catalogue/tests/__init__.py
+++ src/wirecloud/catalogue/tests/__init__.py
@@ -1,6 +1,7 @@
 from wirecloud.catalogue.tests.commands import AddToCatalogueCommandTestCase
 from wirecloud.catalogue.tests.tests import CatalogueAPITestCase, WGTDeploymentTestCase, CatalogueSearchTestCase, CatalogueSuggestionTestCase  # pyflakes:ignore
 from wirecloud.catalogue.tests.south_migrations import CatalogueSouthMigrationsTestCase
+from wirecloud.catalogue.tests.utils import CatalogueUtilsTestCase
 
 from wirecloud.commons.utils.testcases import build_selenium_test_cases
 
