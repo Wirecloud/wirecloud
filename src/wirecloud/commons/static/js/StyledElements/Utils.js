d2e8886c3ac6621211a9e8e422fe49febdb1bb43
Jaime Pajuelo <jpajuelo@conwet.com>
2015-06-02 17:43:15 +0200

    Improve the helper for the class inheritance

--- src/wirecloud/commons/static/js/StyledElements/Utils.js
+++ src/wirecloud/commons/static/js/StyledElements/Utils.js
@@ -713,6 +713,86 @@ if (window.StyledElements == null) {
         }
     };
 
+    var addMembers = function addMembers(constructor, members) {
+
+        for (var name in members) {
+            constructor.prototype[name] = members[name];
+        }
+    };
+
+    var bindMixins = function bindMixins(constructor, mixins) {
+        mixins.forEach(function (mixin) {
+            addMembers(constructor, mixin.prototype);
+        });
+
+        constructor.prototype.mixinClass = function mixinClass(index) {
+            mixins[index].apply(this, Array.prototype.slice.call(arguments, 1));
+        };
+    };
+
+    var inherit = function inherit(constructor, superConstructor) {
+        var counter = 0;
+
+        constructor.prototype = Object.create(superConstructor.prototype);
+
+        addMembers(constructor, {
+
+            constructor: constructor,
+
+            superConstructor: superConstructor,
+
+            superClass: function superClass() {
+                var currentClass = superConstructor;
+
+                for (var i = 0; i < counter; i++) {
+                    currentClass = currentClass.prototype.superConstructor;
+                }
+
+                counter++;
+
+                try {
+                    currentClass.apply(this, Array.prototype.slice.call(arguments));
+                } catch (e) {
+                    counter = 0;
+                    throw e;
+                }
+
+                counter--;
+            },
+
+            superMember: function superMember(name) {
+                var memberArgs = Array.prototype.slice.call(arguments, 1);
+
+                return superConstructor.prototype[name].apply(this, memberArgs);
+            }
+
+        });
+    };
+
+    /**
+     * [defineClass description]
+     *
+     * @param {Object.<String, *>} features [description]
+     * @returns {Function} [description]
+     */
+    Utils.defineClass = function defineClass(features) {
+
+        if ('inherit' in features) {
+            inherit(features.constructor, features.inherit);
+        }
+
+        if ('mixins' in features) {
+            bindMixins(features.constructor, features.mixins);
+        }
+
+        if ('members' in features) {
+            addMembers(features.constructor, features.members);
+        }
+
+        return features.constructor;
+    };
+
+
     var SIZE_UNITS = ['bytes', 'KB', 'MB', 'GB', 'TB'];
     Object.freeze(SIZE_UNITS);
 
