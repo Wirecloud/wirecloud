dac6b82dd55d5acd76c8127dbb08657bf6fa9523
Álvaro Arranz <aarranz@fi.upm.es>
2015-06-10 12:50:17 +0200

    Bump WireCloud version

--- src/wirecloud/platform/__init__.py
+++ src/wirecloud/platform/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011-2014 CoNWeT Lab., Universidad Politécnica de Madrid
+# Copyright (c) 2011-2015 CoNWeT Lab., Universidad Politécnica de Madrid
 
 # This file is part of Wirecloud.
 
@@ -17,5 +17,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
-__version_info__ = (0, 6, 5)
+__version_info__ = (0, 6, 6)
 __version__ = '.'.join(map(str, __version_info__))
