7285d83640382e435e7f49cc4a67921b6d4642e8
Jaime Pajuelo <jpajuelo@conwet.com>
2015-06-11 16:03:08 +0200

    Refactor OffCanvasLayout class

--- src/wirecloud/commons/static/js/StyledElements/OffCanvasLayout.js
+++ src/wirecloud/commons/static/js/StyledElements/OffCanvasLayout.js
@@ -1,24 +1,28 @@
 /*
- *  This file is part of Wirecloud.
- *  Copyright (C) 2015  CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2015 CoNWeT Lab., Universidad Politécnica de Madrid
  *
- *  Wirecloud is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  License, or (at your option) any later version.
+ *     This file is part of Wirecloud Platform.
  *
- *  Wirecloud is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ *     Wirecloud Platform is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+ *     Wirecloud is distributed in the hope that it will be useful, but WITHOUT
+ *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ *     License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Wirecloud Platform.  If not, see
+ *     <http://www.gnu.org/licenses/>.
+ *
  */
 
 /* global StyledElements */
 
 
-(function (ns) {
+(function (se, utils) {
 
     "use strict";
 
@@ -27,142 +31,124 @@
     // ==================================================================================
 
     /**
-     * [OffCanvasLayout description]
+     * Create a new instance of class OffCanvasLayout.
      *
      * @constructor
-     * @param {Object.<String, *>} options [description]
+     * @param {Object.<String, *>} [options] [description]
      */
-    ns.OffCanvasLayout = function OffCanvasLayout(options) {
-        StyledElements.StyledElement.call(this, ns.OffCanvasLayout.EVENTS);
+    se.OffCanvasLayout = utils.defineClass({
 
-        options = StyledElements.Utils.merge(ns.OffCanvasLayout.DEFAULTS, options);
+        constructor: function OffCanvasLayout(options) {
+            options = utils.updateObject(defaults, options);
+            this.superClass(['slideIn', 'slideOut']);
 
-        this.wrapperElement = document.createElement('div');
-        this.wrapperElement.className = "se-offcanvas " + options.sideway + "-sideway";
+            this.wrapperElement = document.createElement('div');
+            this.addClass("se-offcanvas " + options.sideway + "-sideway");
 
-        Object.defineProperties(this, {
-            slipped: {
+            Object.defineProperty(this, 'slipped', {
                 get: function get() {
-                    return this.wrapperElement.classList.contains('slipped');
+                    return this.hasClass('slipped');
                 },
                 set: function set(value) {
-                    if (value) {
-                        this.wrapperElement.classList.add('slipped');
-                    } else {
-                        this.wrapperElement.classList.remove('slipped');
-                    }
+                    this.toggleClass('slipped', value);
                 }
+            });
+
+            this.panels = [];
+            this.latestIndex = 0;
+
+            this.sidebar = new se.Container({
+                extraClass: 'se-offcanvas-sidebar'
+            });
+            this.append(this.sidebar);
+
+            this.content = new se.Container({
+                extraClass: 'se-offcanvas-content'
+            });
+            this.append(this.content);
+
+            this.footer = new se.Container({
+                extraClass: 'se-offcanvas-footer'
+            });
+            this.append(this.footer);
+        },
+
+        inherit: se.StyledElement,
+
+        members: {
+
+            /**
+             * [addPanel description]
+             *
+             * @param {Panel} panel
+             *      [description]
+             * @returns {OffCanvasLayout}
+             *      The instance on which the member is called.
+             */
+            addPanel: function addPanel(panel) {
+                this.sidebar.append(panel);
+                this.panels.push(panel);
+
+                return this;
             },
-            sidebar: {
-                value: new StyledElements.Container({
-                    'class': "se-offcanvas-sidebar"
-                })
+
+            /**
+             * [slideOut description]
+             *
+             * @param {Number} [panelIndex]
+             *      [description]
+             * @returns {OffCanvasLayout}
+             *      The instance on which the member is called.
+             */
+            slideOut: function slideOut(panelIndex) {
+
+                if (panelIndex != null) {
+                    this.latestIndex = panelIndex;
+                }
+
+                this.slipped = false;
+
+                return this.trigger('slideOut', this);
             },
-            content: {
-                value: new StyledElements.Container({
-                    'class': "se-offcanvas-content"
-                })
-            },
-            footer: {
-                value: new StyledElements.Container({
-                    'class': "se-offcanvas-footer"
-                })
+
+            /**
+             * [slideIn description]
+             *
+             * @param {Number} [panelIndex]
+             *      [description]
+             * @returns {OffCanvasLayout}
+             *      The instance on which the member is called.
+             */
+            slideIn: function slideIn(panelIndex) {
+                var panel;
+
+                if (this.panels.length) {
+                    this.panels.forEach(function (panel) {
+                        panel.hide();
+                    });
+
+                    if (panelIndex != null) {
+                        this.latestIndex = panelIndex;
+                    }
+
+                    panel = this.panels[this.latestIndex].show();
+                }
+
+                this.slipped = true;
+
+                return this.trigger('slideIn', panel, this);
             }
-        });
 
-        this.panels = [];
-        this.latestIndex = 0;
+        }
 
-        this.wrapperElement.appendChild(this.sidebar.wrapperElement);
-        this.wrapperElement.appendChild(this.content.wrapperElement);
-        this.wrapperElement.appendChild(this.footer.wrapperElement);
-    };
+    });
 
-    ns.OffCanvasLayout.DEFAULTS = {
+    // ==================================================================================
+    // PRIVATE MEMBERS
+    // ==================================================================================
+
+    var defaults = {
         sideway: 'left'
     };
 
-    ns.OffCanvasLayout.EVENTS = ['show', 'hide'];
-
-    ns.OffCanvasLayout.prototype = new ns.StyledElement();
-
-    // ==================================================================================
-    // PUBLIC MEMBERS
-    // ==================================================================================
-
-    /**
-     * [addPanel description]
-     *
-     * @param {StyledElement} panelElement [description]
-     * @returns {OffCanvasLayout} The instance on which the member is called.
-     */
-    ns.OffCanvasLayout.prototype.addPanel = function addPanel(panelElement) {
-        this.sidebar.appendChild(panelElement.wrapperElement);
-        this.panels.push(panelElement);
-
-        return this;
-    };
-
-    /**
-     * [repaint description]
-     * @override
-     *
-     * @param {[type]} temporal [description]
-     * @returns {OffCanvasLayout} The instance on which the member is called.
-     */
-    ns.OffCanvasLayout.prototype.repaint = function repaint(temporal) {
-        this.sidebar.repaint(temporal);
-        this.content.repaint(temporal);
-        this.footer.repaint(temporal);
-
-        return this;
-    };
-
-    /**
-     * [show description]
-     *
-     * @param {Number} [panelIndex] [description]
-     * @returns {OffCanvasLayout} The instance on which the member is called.
-     */
-    ns.OffCanvasLayout.prototype.show = function show(panelIndex) {
-        var i, shownPanel = null;
-
-        if (this.panels.length) {
-
-            this.panels.forEach(function (panel) {
-                panel.hide();
-            });
-
-            if (typeof panelIndex !== 'undefined') {
-                shownPanel = this.panels[panelIndex].show();
-                this.latestIndex = panelIndex;
-            } else {
-                shownPanel = this.panels[this.latestIndex].show();
-            }
-        }
-
-        this.slipped = true;
-        this.events.show.dispatch(shownPanel);
-
-        return this;
-    };
-
-    /**
-     * [hide description]
-     *
-     * @param {Number} [panelIndex] [description]
-     * @returns {OffCanvasLayout} The instance on which the member is called.
-     */
-    ns.OffCanvasLayout.prototype.hide = function hide(panelIndex) {
-
-        if (typeof panelIndex !== 'undefined') {
-            this.latestIndex = panelIndex;
-        }
-
-        this.slipped = false;
-        this.events.hide.dispatch();
-
-        return this;
-    };
-
-})(StyledElements);
+})(StyledElements, StyledElements.Utils);
