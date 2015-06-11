56f2c2fae1c2b216b69681ebde4c00dd6a03c7e3
Jaime Pajuelo <jpajuelo@conwet.com>
2015-06-11 15:55:20 +0200

    Refactor and add methods to Container class

--- src/wirecloud/commons/static/js/StyledElements/Container.js
+++ src/wirecloud/commons/static/js/StyledElements/Container.js
@@ -19,172 +19,257 @@
  *
  */
 
-/*global StyledElements*/
+/* global StyledElements */
 
-(function () {
+
+(function (se, utils) {
 
     "use strict";
 
+    // ==================================================================================
+    // CLASS DEFINITION
+    // ==================================================================================
+
     /**
-     * Este componente permite crear un contenedor en el que añadir otros
-     * componentes.
+     * Create a new instance of class Container.
      *
-     * @param options
-     * @param events
+     * @constructor
+     * @param {Object.<String, *>} options [description]
+     * @param {String[]} events [description]
      */
-    var Container = function Container(options, events) {
-        var defaultOptions = {
-            'extending': false,
-            'class': '',
-            'useFullHeight': false
-        };
-        options = StyledElements.Utils.merge(defaultOptions, options);
+    se.Container = utils.defineClass({
 
-        // Necesario para permitir herencia
-        if (options.extending) {
-            return;
-        }
+        constructor: function Container(options, events) {
+            options = utils.updateObject(defaults, options);
+            this.superClass(events);
 
-        StyledElements.StyledElement.call(this, events);
+            this.wrapperElement = document.createElement('div');
+            this.children = [];
+            this.useFullHeight = options.useFullHeight;
 
-        this.useFullHeight = options.useFullHeight;
-        this.wrapperElement = document.createElement("div");
-        this.children = [];
-
-        if (options.id) {
-            this.wrapperElement.setAttribute("id", options.id);
-        }
-
-        this.wrapperElement.className = StyledElements.Utils.prependWord(options['class'], "se-container");
-    };
-    Container.prototype = new StyledElements.StyledElement();
-
-    Container.prototype.appendChild = function appendChild(element, refElement) {
-        if (element instanceof StyledElements.StyledElement) {
-            element.insertInto(this.wrapperElement, refElement);
-            this.children.push(element);
-
-            return element;
-        }
-
-        if (typeof element === "string") {
-            element = document.createTextNode(element);
-        }
-
-        if (refElement instanceof StyledElements.StyledElement) {
-            refElement = refElement.wrapperElement;
-        }
-
-        if (refElement != null) {
-            this.wrapperElement.insertBefore(element, refElement);
-        } else {
-            this.wrapperElement.appendChild(element);
-        }
-
-        return element;
-    };
-
-    Container.prototype.removeChild = function removeChild(element) {
-        var index;
-
-        if (element instanceof StyledElements.StyledElement) {
-            index = this.children.indexOf(element);
-            this.children.splice(index, 1);
-            this.wrapperElement.removeChild(element.wrapperElement);
-        } else {
-            this.wrapperElement.removeChild(element);
-        }
-
-        return this;
-    };
-
-    Container.prototype.repaint = function repaint(temporal) {
-        temporal = temporal !== undefined ? temporal : false;
-
-        if (this.useFullHeight) {
-            if (this.wrapperElement.classList.contains('hidden')) {
-                this.wrapperElement.style.height = "";
-                return;
+            if (options.id) {
+                this.wrapperElement.setAttribute('id', options.id);
             }
 
-            var height = this._getUsableHeight();
-            if (height == null) {
-                return; // nothing to do
+            if (options.extraClass) {
+                this.addClass(options.extraClass);
             }
 
-            this.wrapperElement.style.height = (height + "px");
+            if (options['class']) {
+                this.addClass(options['class']);
+            }
+        },
+
+        inherit: se.StyledElement,
+
+        members: {
+
+            /**
+             * @override
+             */
+            append: function append(newElement, refElement) {
+
+                if (newElement instanceof se.StyledElement) {
+                    this.children.push(newElement);
+                }
+
+                return this.superMember('append', newElement, refElement);
+            },
+
+            /**
+             * @override
+             */
+            empty: function empty() {
+                var i = this.children.length
+
+                while (i--) {
+                    this.remove(this.children[i]);
+                }
+
+                return this.superMember('empty');
+            },
+
+            /**
+             * @override
+             */
+            prepend: function prepend(newElement, refElement) {
+
+                if (newElement instanceof se.StyledElement) {
+                    this.children.push(newElement);
+                }
+
+                return this.superMember('prepend', newElement, refElement);
+            },
+
+            /**
+             * @override
+             */
+            remove: function remove(childElement) {
+                var index;
+
+                if (childElement instanceof se.StyledElement) {
+                    if ((index=this.children.indexOf(childElement)) < 0) {
+                        return this;
+                    }
+
+                    this.children.splice(index, 1);
+                }
+
+                return this.superMember('remove', childElement);
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            appendChild: function appendChild(element, refElement) {
+                if (element instanceof StyledElements.StyledElement) {
+                    element.insertInto(this.wrapperElement, refElement);
+                    this.children.push(element);
+
+                    return element;
+                }
+
+                if (typeof element === "string") {
+                    element = document.createTextNode(element);
+                }
+
+                if (refElement instanceof StyledElements.StyledElement) {
+                    refElement = refElement.wrapperElement;
+                }
+
+                if (refElement != null) {
+                    this.wrapperElement.insertBefore(element, refElement);
+                } else {
+                    this.wrapperElement.appendChild(element);
+                }
+
+                return element;
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            removeChild: function removeChild(element) {
+                var index;
+
+                if (element instanceof StyledElements.StyledElement) {
+                    index = this.children.indexOf(element);
+                    this.children.splice(index, 1);
+                    this.wrapperElement.removeChild(element.wrapperElement);
+                } else {
+                    this.wrapperElement.removeChild(element);
+                }
+
+                return this;
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            repaint: function repaint(temporal) {
+                temporal = temporal !== undefined ? temporal : false;
+
+                if (this.useFullHeight) {
+                    if (this.wrapperElement.classList.contains('hidden')) {
+                        this.wrapperElement.style.height = "";
+                        return;
+                    }
+
+                    var height = this._getUsableHeight();
+                    if (height == null) {
+                        return; // nothing to do
+                    }
+
+                    this.wrapperElement.style.height = (height + "px");
+                }
+
+                for (var i = 0; i < this.children.length; i++) {
+                    this.children[i].repaint(temporal);
+                }
+
+                if (this.disabledLayer != null) {
+                    this.disabledLayer.style.height = this.wrapperElement.scrollHeight + 'px';
+                    this.disabledLayer.style.lineHeight = this.wrapperElement.clientHeight + 'px';
+                }
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            clear: function clear() {
+                this.children = [];
+                this.wrapperElement.innerHTML = "";
+                this.wrapperElement.scrollTop = 0;
+                this.wrapperElement.scrollLeft = 0;
+                if (this.disabledLayer != null) {
+                    this.wrapperElement.appendChild(this.disabledLayer);
+                }
+
+                return this;
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            isDisabled: function isDisabled() {
+                return this.disabledLayer != null;
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            setDisabled: function setDisabled(disabled) {
+                if (this.isDisabled() == disabled) {
+                    // Nothing to do
+                    return;
+                }
+
+                if (disabled) {
+                    this.disabledLayer = document.createElement('div');
+                    this.disabledLayer.className = 'se-container-disable-layer';
+
+                    this.disabled_icon = document.createElement('i');
+                    this.disabled_icon.className = 'disable-icon icon-spin icon-spinner';
+                    this.disabledLayer.appendChild(this.disabled_icon);
+
+                    this.wrapperElement.appendChild(this.disabledLayer);
+                    this.disabledLayer.style.height = this.wrapperElement.scrollHeight + 'px';
+                    this.disabledLayer.style.lineHeight = this.wrapperElement.clientHeight + 'px';
+                } else {
+                    this.disabledLayer.parentNode.removeChild(this.disabledLayer);
+                    this.disabledLayer = null;
+                    this.disable_icon = null;
+                }
+                this.enabled = !disabled;
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            enable: function enable() {
+                this.setDisabled(false);
+            },
+
+            /**
+             * @deprecated since version 0.2.0
+             */
+            disable: function disable() {
+                this.setDisabled(true);
+            }
+
         }
 
-        for (var i = 0; i < this.children.length; i++) {
-            this.children[i].repaint(temporal);
-        }
+    });
 
-        if (this.disabledLayer != null) {
-            this.disabledLayer.style.height = this.wrapperElement.scrollHeight + 'px';
-            this.disabledLayer.style.lineHeight = this.wrapperElement.clientHeight + 'px';
-        }
+    // ==================================================================================
+    // PRIVATE MEMBERS
+    // ==================================================================================
+
+    var defaults = {
+        'class': "",
+        id: '',
+        extraClass: "",
+        useFullHeight: false
     };
 
-    /**
-     * Elimina el contenido de este contenedor.
-     */
-    Container.prototype.clear = function clear() {
-        this.children = [];
-        this.wrapperElement.innerHTML = "";
-        this.wrapperElement.scrollTop = 0;
-        this.wrapperElement.scrollLeft = 0;
-        if (this.disabledLayer != null) {
-            this.wrapperElement.appendChild(this.disabledLayer);
-        }
-
-        return this;
-    };
-
-    /**
-     * Devuelve <code>true</code> si este Componente está deshabilitado.
-     */
-    Container.prototype.isDisabled = function isDisabled() {
-        return this.disabledLayer != null;
-    };
-
-    /**
-     * Deshabilita/habilita este contenedor. Cuando un contenedor
-     */
-    Container.prototype.setDisabled = function setDisabled(disabled) {
-        if (this.isDisabled() == disabled) {
-            // Nothing to do
-            return;
-        }
-
-        if (disabled) {
-            this.disabledLayer = document.createElement('div');
-            this.disabledLayer.className = 'se-container-disable-layer';
-
-            this.disabled_icon = document.createElement('i');
-            this.disabled_icon.className = 'disable-icon icon-spin icon-spinner';
-            this.disabledLayer.appendChild(this.disabled_icon);
-
-            this.wrapperElement.appendChild(this.disabledLayer);
-            this.wrapperElement.classList.add('disabled');
-            this.disabledLayer.style.height = this.wrapperElement.scrollHeight + 'px';
-            this.disabledLayer.style.lineHeight = this.wrapperElement.clientHeight + 'px';
-        } else {
-            this.wrapperElement.classList.remove('disabled');
-            this.disabledLayer.parentNode.removeChild(this.disabledLayer);
-            this.disabledLayer = null;
-            this.disable_icon = null;
-        }
-        this.enabled = !disabled;
-    };
-
-    Container.prototype.enable = function enable() {
-        this.setDisabled(false);
-    };
-
-    Container.prototype.disable = function disable() {
-        this.setDisabled(true);
-    };
-
-    StyledElements.Container = Container;
-
-})();
+})(StyledElements, StyledElements.Utils);
