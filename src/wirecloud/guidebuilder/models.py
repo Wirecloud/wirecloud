3aac3cca557f39708c24ab4e5a8e25290aaceb05
Rock Neurotiko <mgarcia@conwet.com>
2015-09-30 13:13:51 +0200

    Added Auth access_token to the model. Fixed screenshots and added new ones.

--- src/wirecloud/guidebuilder/models.py
+++ src/wirecloud/guidebuilder/models.py
@@ -7,3 +7,4 @@ settings.FIWARE_OFFICIAL_PORTAL = True
 settings.FIWARE_APP_ID = "guidebuilder"
 settings.FIWARE_APP_SECRET = "guidebuilder"
 settings.WIRECLOUD_HTTPS_VERIFY = False
+settings.GUIDEBUILDER_AUTH_ACCESS_TOKEN = "YOUR_TOKEN"
