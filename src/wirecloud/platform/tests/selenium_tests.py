3665e12150ecbc7bf11096dd73f4a5ace3e2f409
Álvaro Arranz <aarranz@fi.upm.es>
2015-10-01 23:01:29 +0200

    New test: test_move_widget_and_restore_touch

--- src/wirecloud/platform/tests/selenium_tests.py
+++ src/wirecloud/platform/tests/selenium_tests.py
@@ -25,6 +25,7 @@ from six.moves.urllib.parse import urljoin
 from six.moves.urllib.request import pathname2url
 import time
 
+from django.utils import unittest
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.touch_actions import TouchActions
@@ -993,29 +994,45 @@ class BasicSeleniumTests(WirecloudSeleniumTestCase):
         self.assertEqual(iwidgets[0].layout_position, (0, 0))
         self.assertEqual(iwidgets[1].layout_position, (6, 0))
 
-        self.driver.execute_script('''
-            var layout = Wirecloud.activeWorkspace.getActiveDragboard().baseLayout;
-            var iwidget = Wirecloud.activeWorkspace.getIWidget(%s);
-            layout.initializeMove(iwidget);
-            layout.moveTemporally(6, 1);
-            layout.acceptMove();
-        ''' % iwidgets[0].id);
+        ActionChains(self.driver).click_and_hold(iwidgets[0].title_element).move_by_offset(310, 0).release().perform()
 
         self.assertEqual(iwidgets[0].layout_position, (6, 0))
         self.assertEqual(iwidgets[1].layout_position, (6, 24))
 
-        self.driver.execute_script('''
-            var layout = Wirecloud.activeWorkspace.getActiveDragboard().baseLayout;
-            var iwidget = Wirecloud.activeWorkspace.getIWidget(%s);
-            layout.initializeMove(iwidget);
-            layout.moveTemporally(0, 0);
-            layout.acceptMove();
-        ''' % iwidgets[0].id);
+        ActionChains(self.driver).click_and_hold(iwidgets[0].title_element).move_by_offset(-310, 300).release().perform()
 
         self.assertEqual(iwidgets[0].layout_position, (0, 0))
         self.assertEqual(iwidgets[1].layout_position, (6, 0))
 
     test_move_widget_and_restore.tags = ('wirecloud-selenium', 'wirecloud-dragboard')
+
+    def test_move_widget_and_restore_touch(self):
+
+        if self.driver.capabilities['browserName'] != 'chrome':  # pragma: no cover
+            raise unittest.SkipTest('Touch events are supported only by chrome/chromium')
+
+        self.login(username="user_with_workspaces")
+
+        iwidgets = self.get_current_iwidgets()
+
+        self.assertEqual(iwidgets[0].layout_position, (0, 0))
+        self.assertEqual(iwidgets[1].layout_position, (6, 0))
+
+        iwidgets[0].wait_loaded()
+
+        title_location = iwidgets[0].title_element.location
+        TouchActions(self.driver).tap_and_hold(title_location['x'] + 10, title_location['y'] + 10).move(330, title_location['y'] + 10).release(990, 300).perform()
+
+        self.assertEqual(iwidgets[0].layout_position, (6, 0))
+        self.assertEqual(iwidgets[1].layout_position, (6, 24))
+
+        title_location = iwidgets[0].title_element.location
+        TouchActions(self.driver).tap_and_hold(title_location['x'] + 10, title_location['y'] + 10).move(0, 300).release(0, 300).perform()
+
+        self.assertEqual(iwidgets[0].layout_position, (0, 0))
+        self.assertEqual(iwidgets[1].layout_position, (6, 0))
+
+    test_move_widget_and_restore_touch.tags = ('wirecloud-selenium', 'wirecloud-dragboard')
 
     @uses_extra_resources(('Wirecloud_context-inspector_0.5.wgt',), shared=True)
     def test_basic_add_and_move_widget(self):
