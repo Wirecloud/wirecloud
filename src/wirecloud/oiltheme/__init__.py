e6b4e75f92fde137dfa1d0316d6b5b8f13859c03
Álvaro Arranz <aarranz@fi.upm.es>
2015-11-25 19:42:31 +0100

    Rename wirecloud.oiltheme to wirecloud.fiwarelabtheme maintaining the old name as an alias (but deprecated)

---
+++ src/wirecloud/oiltheme/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+warnings.warn('wirecloud.oiltheme has been deprecated. Please, use wirecloud.fiwarelabtheme instead.', DeprecationWarning)
+
+import wirecloud.fiwarelabtheme
+from wirecloud.fiwarelabtheme import *
+
+__file__ = wirecloud.fiwarelabtheme.__file__
