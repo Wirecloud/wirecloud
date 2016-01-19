dcb27f5b0035e7b03920ef96f51827a23ce3d213
Álvaro Arranz <aarranz@fi.upm.es>
2016-01-19 11:07:48 +0100

    Fix quick_start flag of the startproject command on Django 1.7+. See #84

--- src/wirecloud/commons/commands/startproject.py
+++ src/wirecloud/commons/commands/startproject.py
@@ -85,7 +85,10 @@ class StartprojectCommand(BaseCommand):
         if options['quick_start']:
 
             os.chdir(project_name)
-            exec_external_python_cmd('manage.py syncdb --migrate --noinput')
+            if django.VERSION[1] >= 7:
+                exec_external_python_cmd('manage.py migrate --noinput')
+            else:
+                exec_external_python_cmd('manage.py syncdb --migrate --noinput')
             exec_external_python_cmd('manage.py loaddata wirecloud_quick_start')
             exec_external_python_cmd('manage.py collectstatic --noinput')
             exec_external_python_cmd('manage.py compress --force')
