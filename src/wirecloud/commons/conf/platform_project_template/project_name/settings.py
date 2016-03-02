eaa8543941c82f570443e2ad7715e145a9126699
Álvaro Arranz <aarranz@fi.upm.es>
2016-03-02 08:48:12 +0100

    Remove outdated comments as WireCloud requires Django 1.5+

--- src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
+++ src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
@@ -29,7 +29,6 @@ DATABASES = {
     }
 }
 
-# This setting has only effect in DJango 1.5+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
