caf2cf48034c0d073065d9aae9ff7f00f43905b5
Jaime Pajuelo <jpajuelo@conwet.com>
2016-03-04 02:58:49 +0100

    Add se helper to prepend child nodes

--- src/wirecloud/commons/static/js/StyledElements/Utils.js
+++ src/wirecloud/commons/static/js/StyledElements/Utils.js
@@ -1079,6 +1079,40 @@ if (window.StyledElements == null) {
         });
     };
 
+    /**
+     * Insert the `newElement` either to the beginning of `parentElement` or
+     * before the `refElement` given.
+     * @since 0.7
+     *
+     * @param {(StyledElements.StyledElement|Node)} parentElement
+     *     An element to be the parent of the `newElement`.
+     * @param {(StyledElements.StyledElement|Node|String)} newElement
+     *     An element to insert into the `parentElement`.
+     * @param {(StyledElements.StyledElement|Node)} [refElement]
+     *     Optional. An element before which `newElement` is inserted.
+     *
+     * @returns {Array.<(StyledElements.StyledElement|Node)>}
+     *     The new elements that were inserted.
+     */
+    Utils.prependChild = function prependChild(parentElement, newElement, refElement) {
+        var parentNode = getNode(parentElement);
+        var refNode = getNode(refElement);
+
+        if (typeof newElement === 'string') {
+            newElement = document.createTextNode(newElement);
+        }
+
+        return getNodes(newElement).map(function (childElement) {
+            parentNode.insertBefore(getNode(childElement), refNode == null ? parentNode.firstChild : refNode);
+
+            if (parentElement instanceof StyledElements.StyledElement && childElement instanceof StyledElements.StyledElement) {
+                childElement.parentElement = parentElement;
+            }
+
+            return childElement;
+        });
+    };
+
     var getNode = function getNode(value) {
         return value instanceof StyledElements.StyledElement ? value.get() : value;
     };
