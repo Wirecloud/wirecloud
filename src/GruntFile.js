03524650ee444d4454b62e73ee747b941c0c948a
Álvaro Arranz <aarranz@fi.upm.es>
2016-03-10 17:17:44 +0100

    Fix GruntFile.js

--- src/GruntFile.js
+++ src/GruntFile.js
@@ -1,75 +1,84 @@
-/*!
- *   Copyright 2014-2015 CoNWeT Lab., Universidad Politecnica de Madrid
+/*
+ *     Copyright (c) 2016 CoNWeT Lab., Universidad Politécnica de Madrid
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     This file is part of Wirecloud Platform.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     Wirecloud Platform is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ *     Wirecloud is distributed in the hope that it will be useful, but WITHOUT
+ *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ *     License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Wirecloud Platform.  If not, see
+ *     <http://www.gnu.org/licenses/>.
+ *
  */
 
+/*
+ * This GruntFile is only used for building the StyledElements minimizied
+ * version and passing the tests for this framework
+ */
 
 var styledElementsFiles = [
-    'Utils.js',
-    'Event.js',
-    'ObjectWithEvents.js',
-    'StyledElements.js',
-    'InputElement.js',
-    'CommandQueue.js',
-    'Container.js',
-    'Addon.js',
-    'Accordion.js',
-    'Expander.js',
-    'Fragment.js',
-    'PaginatedSource.js',
-    'GUIBuilder.js',
-    'Tooltip.js',
-    'Button.js',
-    'FileButton.js',
-    'PopupMenuBase.js',
-    'PopupMenu.js',
-    'DynamicMenuItems.js',
-    'MenuItem.js',
-    'Separator.js',
-    'SubMenuItem.js',
-    'PopupButton.js',
-    'StaticPaginatedSource.js',
-    'FileField.js',
-    'NumericField.js',
-    'TextField.js',
-    'TextArea.js',
-    'List.js',
-    'PasswordField.js',
-    'Select.js',
-    'ToggleButton.js',
-    'Pills.js',
-    'Tab.js',
-    'Notebook.js',
-    'Alternative.js',
-    'Alternatives.js',
-    'HorizontalLayout.js',
-    'BorderLayout.js',
-    'ModelTable.js',
-    'EditableElement.js',
-    'HiddenField.js',
-    'ButtonsGroup.js',
-    'CheckBox.js',
-    'RadioButton.js',
-    'InputInterface.js',
-    'TextInputInterface.js',
-    'InputInterfaces.js',
-    'VersionInputInterface.js',
-    'InputInterfaceFactory.js',
-    'DefaultInputInterfaceFactory.js',
-    'Form.js',
-    'PaginationInterface.js',
-    'Popover.js'
+    'wirecloud/commons/static/js/StyledElements/Utils.js',
+    'wirecloud/commons/static/js/StyledElements/Event.js',
+    'wirecloud/commons/static/js/StyledElements/ObjectWithEvents.js',
+    'wirecloud/commons/static/js/StyledElements/StyledElements.js',
+    'wirecloud/commons/static/js/StyledElements/InputElement.js',
+    'wirecloud/commons/static/js/StyledElements/CommandQueue.js',
+    'wirecloud/commons/static/js/StyledElements/Container.js',
+    'wirecloud/commons/static/js/StyledElements/Addon.js',
+    'wirecloud/commons/static/js/StyledElements/Accordion.js',
+    'wirecloud/commons/static/js/StyledElements/Expander.js',
+    'wirecloud/commons/static/js/StyledElements/Fragment.js',
+    'wirecloud/commons/static/js/StyledElements/PaginatedSource.js',
+    'wirecloud/commons/static/js/StyledElements/GUIBuilder.js',
+    'wirecloud/commons/static/js/StyledElements/Tooltip.js',
+    'wirecloud/commons/static/js/StyledElements/Button.js',
+    'wirecloud/commons/static/js/StyledElements/FileButton.js',
+    'wirecloud/commons/static/js/StyledElements/PopupMenuBase.js',
+    'wirecloud/commons/static/js/StyledElements/PopupMenu.js',
+    'wirecloud/commons/static/js/StyledElements/DynamicMenuItems.js',
+    'wirecloud/commons/static/js/StyledElements/MenuItem.js',
+    'wirecloud/commons/static/js/StyledElements/Separator.js',
+    'wirecloud/commons/static/js/StyledElements/SubMenuItem.js',
+    'wirecloud/commons/static/js/StyledElements/PopupButton.js',
+    'wirecloud/commons/static/js/StyledElements/StaticPaginatedSource.js',
+    'wirecloud/commons/static/js/StyledElements/FileField.js',
+    'wirecloud/commons/static/js/StyledElements/NumericField.js',
+    'wirecloud/commons/static/js/StyledElements/TextField.js',
+    'wirecloud/commons/static/js/StyledElements/TextArea.js',
+    'wirecloud/commons/static/js/StyledElements/List.js',
+    'wirecloud/commons/static/js/StyledElements/PasswordField.js',
+    'wirecloud/commons/static/js/StyledElements/Select.js',
+    'wirecloud/commons/static/js/StyledElements/ToggleButton.js',
+    'wirecloud/commons/static/js/StyledElements/Pills.js',
+    'wirecloud/commons/static/js/StyledElements/Tab.js',
+    'wirecloud/commons/static/js/StyledElements/Notebook.js',
+    'wirecloud/commons/static/js/StyledElements/Alternative.js',
+    'wirecloud/commons/static/js/StyledElements/Alternatives.js',
+    'wirecloud/commons/static/js/StyledElements/HorizontalLayout.js',
+    'wirecloud/commons/static/js/StyledElements/BorderLayout.js',
+    'wirecloud/commons/static/js/StyledElements/ModelTable.js',
+    'wirecloud/commons/static/js/StyledElements/EditableElement.js',
+    'wirecloud/commons/static/js/StyledElements/HiddenField.js',
+    'wirecloud/commons/static/js/StyledElements/ButtonsGroup.js',
+    'wirecloud/commons/static/js/StyledElements/CheckBox.js',
+    'wirecloud/commons/static/js/StyledElements/RadioButton.js',
+    'wirecloud/commons/static/js/StyledElements/InputInterface.js',
+    'wirecloud/commons/static/js/StyledElements/TextInputInterface.js',
+    'wirecloud/commons/static/js/StyledElements/InputInterfaces.js',
+    'wirecloud/commons/static/js/StyledElements/VersionInputInterface.js',
+    'wirecloud/commons/static/js/StyledElements/InputInterfaceFactory.js',
+    'wirecloud/commons/static/js/StyledElements/DefaultInputInterfaceFactory.js',
+    'wirecloud/commons/static/js/StyledElements/Form.js',
+    'wirecloud/commons/static/js/StyledElements/PaginationInterface.js',
+    'wirecloud/commons/static/js/StyledElements/Popover.js'
 ];
 
 
@@ -82,7 +91,7 @@ module.exports = function (grunt) {
         uglify: {
             my_target: {
                 files: {
-                    'StyledElements.min.js': styledElementsFiles
+                    'dist/StyledElements.min.js': styledElementsFiles
                 }
             }
         },
@@ -92,7 +101,7 @@ module.exports = function (grunt) {
             test: {
                 src: styledElementsFiles,
                 options: {
-                    specs: 'StyledElementsSpec.js'
+                    specs: 'js_tests/StyledElementsSpec.js'
                 }
             },
 
