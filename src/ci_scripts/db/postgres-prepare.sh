1102212489f1fdd1ee233383cc189a028541fe4e
Álvaro Arranz <aarranz@fi.upm.es>
2016-04-20 11:34:17 +0200

    Fix postgres ci scripts

--- src/ci_scripts/db/postgres-prepare.sh
+++ src/ci_scripts/db/postgres-prepare.sh
@@ -3,8 +3,8 @@ export DB_NAME=${DB_USER}
 
 pip install psycopg2
 
-createuser -U postgres -h ${DB_HOST_IP} -p ${DB_HOST_PORT} ${DB_USER}
-createdb -U postgres -h ${DB_HOST_IP} -p ${DB_HOST_PORT} -O ${DB_USER} ${DB_NAME}
-createdb -U postgres -h ${DB_HOST_IP} -p ${DB_HOST_PORT} -O ${DB_USER} test_${DB_NAME}
+createuser -U postgres -h ${DB_HOST} -p ${DB_PORT} ${DB_USER}
+createdb -U postgres -h ${DB_HOST} -p ${DB_PORT} -O ${DB_USER} ${DB_NAME}
+createdb -U postgres -h ${DB_HOST} -p ${DB_PORT} -O ${DB_USER} test_${DB_NAME}
 
 cat ${WORKSPACE}/src/ci_scripts/db/postgres-conf.template >> $1/settings.py
