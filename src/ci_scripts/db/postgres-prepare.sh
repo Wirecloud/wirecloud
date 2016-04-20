4c57b783fe25b5e399d3d33d2fb9fbe9c0a9a4c5
Álvaro Arranz <aarranz@fi.upm.es>
2016-04-20 14:21:48 +0200

    Use the postgres (admin) user for passing the CI tests

--- src/ci_scripts/db/postgres-prepare.sh
+++ src/ci_scripts/db/postgres-prepare.sh
@@ -1,10 +1,8 @@
-export DB_USER=wirecloud_ci
-export DB_NAME=${DB_USER}
+export DB_USER=postgres
+export DB_NAME=wirecloud_ci
 
 pip install psycopg2
 
-createuser -U postgres -h ${DB_HOST} -p ${DB_PORT} ${DB_USER}
 createdb -U postgres -h ${DB_HOST} -p ${DB_PORT} -O ${DB_USER} ${DB_NAME}
-createdb -U postgres -h ${DB_HOST} -p ${DB_PORT} -O ${DB_USER} test_${DB_NAME}
 
 cat ${WORKSPACE}/src/ci_scripts/db/postgres-conf.template >> $1/settings.py
