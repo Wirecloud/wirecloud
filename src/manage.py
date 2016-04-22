0983715cd2ee4eb3ac411e1ff24fa2e49df54eb5
Álvaro Arranz <aarranz@fi.upm.es>
2016-04-22 12:33:44 +0200

    Allow to tests ETags when using the runserver command

--- src/manage.py
+++ src/manage.py
@@ -4,6 +4,11 @@ import sys
 
 if __name__ == "__main__":
 
+    # Browsers doesn't use content negotiation using ETags with HTTP 1.0 servers
+    # Force Django to use HTTP 1.1 when using the runserver command
+    from wsgiref import simple_server
+    simple_server.ServerHandler.http_version = "1.1"
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
