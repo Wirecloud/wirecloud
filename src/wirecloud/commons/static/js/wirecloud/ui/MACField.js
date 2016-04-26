1f485f871613fdb3290c00aec38bcb7a04a08039
Álvaro Arranz <aarranz@fi.upm.es>
2016-04-26 16:18:45 +0200

    Initial se-input-group support

--- src/wirecloud/commons/static/js/wirecloud/ui/MACField.js
+++ src/wirecloud/commons/static/js/wirecloud/ui/MACField.js
@@ -1,5 +1,5 @@
 /*
- *     Copyright (c) 2014-2015 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2014-2016 CoNWeT Lab., Universidad Politécnica de Madrid
  *
  *     This file is part of Wirecloud Platform.
  *
