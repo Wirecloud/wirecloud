d1c9be202956a2a95cf116ff6f881bd61e88175c
Álvaro Arranz <aarranz@fi.upm.es>
2016-06-29 16:55:55 +0200

    Update FIWARE Lab footer

--- src/wirecloud/fiwarelabtheme/static/js/wirecloud/ui/WirecloudHeader.js
+++ src/wirecloud/fiwarelabtheme/static/js/wirecloud/ui/WirecloudHeader.js
@@ -255,7 +255,7 @@
 
         this.footer = document.createElement('footer');
 
-        msg = '<div class="container-fluid"><div class="footer-content"><div class="cred">2016 © <a href="http://fiware.org/" target="_blank">FIWARE</a>. The use of FIWARE %(environment)s services is subject to the acceptance of the <a href="https://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/FIWARE_LAB_Terms_and_Conditions" target="_blank">Terms and Conditions</a>, <a href="http://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/FI-LAB_Personal_Data_Protection_Policy" target="_blank">Personal Data Protection Policy</a> and <a href="http://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/Cookies_Policy_FIWARE_Lab" target="_blank">Cookies Policy</a>.</div></div></div>';
+        msg = '<div class="container-fluid"><div class="footer-content"><div class="cred">2016 © <a href="http://fiware.org/" target="_blank">FIWARE</a>. The use of FIWARE %(environment)s services is subject to the acceptance of the <a href="https://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/FIWARE_LAB_Terms_and_Conditions" target="_blank">Terms and Conditions</a>, <a href="http://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/FIWARE_Privacy_Policy" target="_blank">Privacy Policy</a> and <a href="http://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/Cookies_Policy_FIWARE" target="_blank">Cookies Policy</a>.</div></div></div>';
         if (Wirecloud.constants.FIWARE_IDM_SERVER === 'https://account.lab.fiware.org') {
             environment = 'Lab';
         } else {
