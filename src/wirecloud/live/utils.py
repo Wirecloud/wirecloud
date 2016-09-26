9009315381edd69adac3319b973b3bcdb16f23e4
Álvaro Arranz <aarranz@fi.upm.es>
2016-09-26 18:47:31 +0200

    Add missing module wirecloud.live.utils

---
+++ src/wirecloud/live/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of Wirecloud.
+
+# Wirecloud is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wirecloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from base64 import b64encode
+
+
+def build_group_name(name):
+    return b"wc-%s" % b64encode(name.encode('utf-8'), b'-_').replace(b'=', b'.')
+
+
+WIRECLOUD_BROADCAST_GROUP = build_group_name('live-*')
