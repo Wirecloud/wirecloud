78fc30cdfbcc837c8d5c8e7d9040e784bcaa6188
Jaime Pajuelo <jpajuelo@conwet.com>
2016-10-05 11:33:38 +0200

    Add KeywordSuggestion tests to remove endpoints

--- src/js_tests/wirecloud/KeywordSuggestionSpec.js
+++ src/js_tests/wirecloud/KeywordSuggestionSpec.js
@@ -64,6 +64,104 @@
                 expect(engine.endpoints.source.list.length).toBe(1);
             });
         });
+
+        describe("removeEndpoint(endpoint)", function () {
+            var engine;
+
+            beforeEach(function () {
+                // Provide a default instance of Select for testing
+                engine = new ns.KeywordSuggestion();
+            });
+
+            it("should remove endpoints", function () {
+                var endpoint1 = new Wirecloud.ui.WiringEditor.Endpoint("source",
+                    {
+                        id: "widget/19/condition-list",
+                        friendcode: "list",
+                        name: "",
+                        description: "",
+                        label: ""
+                    },
+                    {
+                        id: "CoNWeT/jenkins-project-build-list/0.1.9",
+                        type: "widget",
+                        equals: function () {return false;}
+                    }
+                );
+                var endpoint2 = new Wirecloud.ui.WiringEditor.Endpoint("target",
+                    {
+                        id: "operator/1/list",
+                        friendcode: "list",
+                        name: "",
+                        description: "",
+                        label: ""
+                    },
+                    {
+                        id: "CoNWeT/example-1/1.0",
+                        type: "operator",
+                        equals: function () {return false;}
+                    }
+                );
+                var endpoint3 = new Wirecloud.ui.WiringEditor.Endpoint("target",
+                    {
+                        id: "operator/1/list-sorted",
+                        friendcode: "list list-sorted",
+                        name: "",
+                        description: "",
+                        label: ""
+                    },
+                    {
+                        id: "CoNWeT/example-1/1.0",
+                        type: "operator",
+                        equals: function () {return false;}
+                    }
+                );
+                var callback = jasmine.createSpy('callback');
+
+                engine
+                    .appendEndpoint(endpoint1)
+                    .appendEndpoint(endpoint2)
+                    .appendEndpoint(endpoint3)
+                    .removeEndpoint(endpoint3);
+
+                expect(Object.keys(engine.endpoints.target)).toEqual(['list']);
+
+                engine.forEachSuggestion(endpoint1, callback);
+                expect(callback.calls.count()).toEqual(1);
+
+                engine
+                    .removeEndpoint(endpoint2);
+                callback.calls.reset();
+
+                expect(Object.keys(engine.endpoints.target)).toEqual([]);
+
+                engine.forEachSuggestion(endpoint1, callback);
+                expect(callback.calls.count()).toEqual(0);
+            });
+
+            it("should remove missing endpoints", function () {
+                var endpoint1 = new Wirecloud.ui.WiringEditor.Endpoint("source",
+                    {
+                        id: "widget/19/condition-list",
+                        friendcode: "list",
+                        name: "",
+                        description: "",
+                        label: "",
+                        missing: true
+                    },
+                    {
+                        id: "CoNWeT/jenkins-project-build-list/0.1.9",
+                        type: "widget"
+                    }
+                );
+
+                engine
+                    .appendEndpoint(endpoint1)
+                    .removeEndpoint(endpoint1);
+
+                expect(engine.endpoints.source.list).toBeUndefined();
+            });
+        });
     });
  
 })(Wirecloud.wiring);
