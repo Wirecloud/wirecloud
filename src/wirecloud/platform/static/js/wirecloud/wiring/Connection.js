c85df8fae975f132a1391647b6f5cceba8f049af
Jaime Pajuelo <jpajuelo@conwet.com>
2016-10-07 16:05:46 +0200

    Remove the defineClass method for connections

--- src/wirecloud/platform/static/js/wirecloud/wiring/Connection.js
+++ src/wirecloud/platform/static/js/wirecloud/wiring/Connection.js
@@ -44,145 +44,139 @@
      * @param {Wiring} wiringEngine
      *      [TODO: description]
      */
-    ns.Connection = utils.defineClass({
+    ns.Connection = function Connection(wiring, source, target, options) {
+        se.ObjectWithEvents.call(this, ['detach', 'establish', 'remove']);
+        this.established = false;
 
-        constructor: function Connection(wiring, source, target, options) {
-            this.superClass(['detach', 'establish', 'remove']);
+        options = utils.merge({
+            readonly: false
+        }, options);
+
+        Object.defineProperties(this, {
+            id: {
+                get: function get() {return this.source.id + "//" + this.target.id;}
+            },
+            logManager: {value: new ns.ConnectionLogManager(this, wiring)},
+            readonly: {
+                value: options.readonly
+            },
+            source: {value: source, writable: true},
+            target: {value: target, writable: true},
+            volatile: {value: source.component.volatile || target.component.volatile},
+            wiring: {
+                value: wiring
+            }
+        });
+    };
+
+    ns.Connection.JSON_TEMPLATE = {
+        readonly: false,
+        source: {
+            type: "",
+            id: null,
+            endpoint: ""
+        },
+        target: {
+            type: "",
+            id: null,
+            endpoint: ""
+        }
+    };
+
+    // =========================================================================
+    // PUBLIC MEMBERS
+    // =========================================================================
+
+    utils.inherit(ns.Connection, se.ObjectWithEvents, {
+
+        _connect: function _connect() {
+            var message = utils.gettext("The connection ('%(source)s'-'%(target)s') was established.");
+
+            this.established = true;
+            this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.INFO_MSG);
+
+            return this.dispatchEvent('establish');
+        },
+
+        _disconnect: function _disconnect() {
+            var message = utils.gettext("The connection ('%(source)s'-'%(target)s') was detached.");
+
+            this.established = false;
+            this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.INFO_MSG);
+
+            return this.dispatchEvent('detach');
+        },
+
+        detach: function detach() {
+
+            if (!this.established) {
+                return this;
+            }
+
+            this.source.disconnect(this.target);
             this.established = false;
 
-            options = utils.merge({
-                readonly: false
-            }, options);
-
-            Object.defineProperties(this, {
-                id: {
-                    get: function get() {return this.source.id + "//" + this.target.id;}
-                },
-                logManager: {value: new ns.ConnectionLogManager(this, wiring)},
-                readonly: {
-                    value: options.readonly
-                },
-                source: {value: source, writable: true},
-                target: {value: target, writable: true},
-                volatile: {value: source.component.volatile || target.component.volatile},
-                wiring: {
-                    value: wiring
-                }
-            });
+            return this;
         },
 
-        inherit: se.ObjectWithEvents,
-
-        statics: {
-
-            JSON_TEMPLATE: {
-                readonly: false,
-                source: {
-                    type: "",
-                    id: null,
-                    endpoint: ""
-                },
-                target: {
-                    type: "",
-                    id: null,
-                    endpoint: ""
-                }
-            }
-
+        equals: function equals(connection) {
+            return connection instanceof ns.Connection && this.id === connection.id;
         },
 
-        members: {
+        establish: function establish() {
 
-            _connect: function _connect() {
-                var message = utils.gettext("The connection ('%(source)s'-'%(target)s') was established.");
-
-                this.established = true;
-                this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.INFO_MSG);
-
-                return this.dispatchEvent('establish');
-            },
-
-            _disconnect: function _disconnect() {
-                var message = utils.gettext("The connection ('%(source)s'-'%(target)s') was detached.");
-
-                this.established = false;
-                this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.INFO_MSG);
-
-                return this.dispatchEvent('detach');
-            },
-
-            detach: function detach() {
-
-                if (!this.established) {
-                    return this;
-                }
-
-                this.source.disconnect(this.target);
-                this.established = false;
-
+            if (this.source.missing || this.target.missing) {
+                var message = utils.gettext("The connection ('%(source)s'-'%(target)s') has a missing endpoint.");
+                this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.ERROR_MSG);
                 return this;
-            },
-
-            equals: function equals(connection) {
-                return connection instanceof ns.Connection && this.id === connection.id;
-            },
-
-            establish: function establish() {
-
-                if (this.source.missing || this.target.missing) {
-                    var message = utils.gettext("The connection ('%(source)s'-'%(target)s') has a missing endpoint.");
-                    this.logManager.log(utils.interpolate(message, this), Wirecloud.constants.LOGGING.ERROR_MSG);
-                    return this;
-                }
-
-                if (this.established) {
-                    return this;
-                }
-
-                this.source.connect(this.target, this);
-                this.established = true;
-
-                return this;
-            },
-
-            updateEndpoint: function updateEndpoint(endpoint) {
-
-                if (!(endpoint instanceof Wirecloud.wiring.Endpoint)) {
-                    throw new TypeError("endpoint must be a Wirecloud.wiring.Endpoint instance");
-                }
-
-                this.detach();
-                this.logManager.newCycle();
-
-                if (endpoint instanceof Wirecloud.wiring.SourceEndpoint) {
-                    this.source = endpoint;
-                } else /* if (endpoint instanceof Wirecloud.wiring.TargetEndpoint) */ {
-                    this.target = endpoint;
-                }
-
-                return this.establish();
-            },
-
-            remove: function remove() {
-                this.detach();
-                return this.dispatchEvent('remove');
-            },
-
-            showLogs: function showLogs() {
-                var modal = new Wirecloud.ui.LogWindowMenu(this.logManager);
-                modal.show();
-
-                return this;
-            },
-
-            toJSON: function toJSON() {
-                return JSON.parse(JSON.stringify({
-                    readonly: this.readonly,
-                    source: this.source,
-                    target: this.target
-                }));
             }
 
+            if (this.established) {
+                return this;
+            }
+
+            this.source.connect(this.target, this);
+            this.established = true;
+
+            return this;
+        },
+
+        updateEndpoint: function updateEndpoint(endpoint) {
+
+            if (!(endpoint instanceof Wirecloud.wiring.Endpoint)) {
+                throw new TypeError("endpoint must be a Wirecloud.wiring.Endpoint instance");
+            }
+
+            this.detach();
+            this.logManager.newCycle();
+
+            if (endpoint instanceof Wirecloud.wiring.SourceEndpoint) {
+                this.source = endpoint;
+            } else /* if (endpoint instanceof Wirecloud.wiring.TargetEndpoint) */ {
+                this.target = endpoint;
+            }
+
+            return this.establish();
+        },
+
+        remove: function remove() {
+            this.detach();
+            return this.dispatchEvent('remove');
+        },
+
+        showLogs: function showLogs() {
+            var modal = new Wirecloud.ui.LogWindowMenu(this.logManager);
+            modal.show();
+
+            return this;
+        },
+
+        toJSON: function toJSON() {
+            return JSON.parse(JSON.stringify({
+                readonly: this.readonly,
+                source: this.source,
+                target: this.target
+            }));
         }
 
     });
