962513ffe748a022e9505638de224ac0156f96b8
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2017-07-14 13:40:11 +0200

    Add Solr schema template and use the TEST_HAYSTACK_CONNECTIONS setting

--- src/ci_scripts/conf_scripts/solr-prepare.sh
+++ src/ci_scripts/conf_scripts/solr-prepare.sh
@@ -6,3 +6,10 @@ sed '34,39d' ${WC_INSTANCE_NAME}/settings.py
 
 # Add ElasticSearch Haystack settings
 cat ${WORKSPACE}/src/ci_scripts/templates/solr-conf.template >> ${WC_INSTANCE_NAME}/settings.py
+
+# Upload Solr schema
+apt-get install -y sshpass
+sshpass -p 'wirecloud' scp -r ${WORKSPACE}/src/ci_scripts/templates/solr-schema.template solr@${SOLR_SERVER}:/opt/solr/server/solr/tester/conf/schema.xml
+
+# Reload the Solr core
+curl "http://${SOLR_SERVER}:8983/solr/admin/cores?action=RELOAD&core=tester"
