aab3a86bead788f374f6b234ca56c0eb71e199b7
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2017-07-17 13:42:34 +0200

    Improve solr-prepare: remove previous Solr schemas

--- src/ci_scripts/conf_scripts/solr-prepare.sh
+++ src/ci_scripts/conf_scripts/solr-prepare.sh
@@ -9,6 +9,7 @@ cat ${WORKSPACE}/src/ci_scripts/templates/solr-conf.template >> ${WC_INSTANCE_NA
 
 # Upload Solr schema
 apt-get install -y sshpass
+sshpass -p 'wirecloud' ssh solr@${SOLR_SERVER} "rm -f /opt/solr/server/solr/tester/conf/schema.xml /opt/solr/server/solr/tester/conf/managed-schema"
 sshpass -p 'wirecloud' scp -r ${WORKSPACE}/src/ci_scripts/templates/solr-schema.template solr@${SOLR_SERVER}:/opt/solr/server/solr/tester/conf/schema.xml
 
 # Reload the Solr core
