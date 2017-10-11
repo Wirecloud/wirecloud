d8fc815397f93600f225058f2205d89706de0b95
Álvaro Arranz <aarranz@ficodes.com>
2017-10-11 10:31:01 +0200

    Fix import typo

--- src/wirecloud/platform/localcatalogue/views.py
+++ src/wirecloud/platform/localcatalogue/views.py
@@ -35,7 +35,7 @@ from wirecloud.catalogue.models import CatalogueResource
 import wirecloud.catalogue.utils as catalogue_utils
 from wirecloud.commons.baseviews import Resource
 from wirecloud.commons.utils.http import authentication_required, authentication_required_cond, build_downloadfile_response, build_error_response, get_content_type, normalize_boolean_param, consumes, parse_json_request, produces
-from wirecloud.commons.utils.structure import CaseInsensitiveDict
+from wirecloud.commons.utils.structures import CaseInsensitiveDict
 from wirecloud.commons.utils.template import TemplateParseException, UnsupportedFeature
 from wirecloud.commons.utils.transaction import commit_on_http_success
 from wirecloud.commons.utils.wgt import InvalidContents, WgtFile
