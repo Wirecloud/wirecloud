61e867aa7306c10416d9a3697bb9cf3764bd2763
Álvaro Arranz <aarranz@ficodes.com>
2017-10-23 09:53:59 +0200

    Minor improvements

--- src/wirecloud/commons/static/js/StyledElements/StyledElements.js
+++ src/wirecloud/commons/static/js/StyledElements/StyledElements.js
@@ -1,5 +1,5 @@
 /*
- *     Copyright (c) 2008-2015 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2008-2017 CoNWeT Lab., Universidad Politécnica de Madrid
  *
  *     This file is part of Wirecloud Platform.
  *
