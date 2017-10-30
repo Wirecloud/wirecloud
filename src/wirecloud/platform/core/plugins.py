71acc6e514a9505a375658f378da55efb36959a1
Álvaro Arranz <aarranz@ficodes.com>
2017-10-30 09:57:57 +0100

    Bump WireCloud versions. See #309

--- src/wirecloud/platform/core/plugins.py
+++ src/wirecloud/platform/core/plugins.py
@@ -256,7 +256,7 @@ class WirecloudCorePlugin(WirecloudPlugin):
 
     features = {
         'Wirecloud': wirecloud.platform.__version__,
-        'ApplicationMashup': '1.0',
+        'ApplicationMashup': wirecloud.platform.__application_mashup_version__,
         'StyledElements': '0.9',
         'FullscreenWidget': '0.5',
         'DashboardManagement': '1.0',
