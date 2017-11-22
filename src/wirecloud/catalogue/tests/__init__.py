09eaa85bb2891f865a2f92b426298f9159f982ce
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2017-11-22 11:16:08 +0100

    Restore noqa comments

--- src/wirecloud/catalogue/tests/__init__.py
+++ src/wirecloud/catalogue/tests/__init__.py
@@ -1,4 +1,4 @@
-from wirecloud.catalogue.tests.commands import AddToCatalogueCommandTestCase
-from wirecloud.catalogue.tests.tests import CatalogueAPITestCase, WGTDeploymentTestCase, CatalogueSearchTestCase, CatalogueMediaTestCase  # pyflakes:ignore
-from wirecloud.catalogue.tests.utils import CatalogueUtilsTestCase
-from wirecloud.catalogue.tests.selenium import *
+from wirecloud.catalogue.tests.commands import AddToCatalogueCommandTestCase # noqa
+from wirecloud.catalogue.tests.tests import CatalogueAPITestCase, WGTDeploymentTestCase, CatalogueSearchTestCase, CatalogueMediaTestCase # noqa
+from wirecloud.catalogue.tests.utils import CatalogueUtilsTestCase # noqa
+from wirecloud.catalogue.tests.selenium import * # noqa
