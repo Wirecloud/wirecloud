52a922554a2f205e27d36da84609dd2082efbecd
Álvaro Arranz <aarranz@ficodes.com>
2018-06-17 20:47:45 +0200

    Fix tab scrolling on Notebooks (through the focus, shiftRightTabs and shiftLeftTabs methods).

--- src/wirecloud/platform/core/plugins.py
+++ src/wirecloud/platform/core/plugins.py
@@ -256,7 +256,7 @@ class WirecloudCorePlugin(WirecloudPlugin):
     features = {
         'Wirecloud': wirecloud.platform.__version__,
         'ApplicationMashup': wirecloud.platform.__application_mashup_version__,
-        'StyledElements': '0.9',
+        'StyledElements': '0.9.1',
         'FullscreenWidget': '0.5',
         'DashboardManagement': '1.0',
         'ComponentManagement': '1.0',
