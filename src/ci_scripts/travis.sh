2675f0be3aba91db5eab0220dbbc748edc2037da
Álvaro Arranz <aarranz@ficodes.com>
2018-06-24 21:28:50 +0200

    CI: Enable selenium tests on Travis

--- src/ci_scripts/travis.sh
+++ src/ci_scripts/travis.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "${TEST_SUITE}" == "js_unittests" ]; then
     cd $TRAVIS_BUILD_DIR/src
     grunt ci
     exit
+elif [ "${TEST_SUITE}" == "selenium" ]; then
+    FLAGS="postgres django${DJANGO_VERSION} selenium"
+else
+    FLAGS="sqlite3 django${DJANGO_VERSION} unittest"
 fi
 
 
@@ -11,12 +17,11 @@ export WC_INSTANCE_NAME=test_instance
 [ -n "${IP_ADDR}" ] || IP_ADDR="localhost"
 
 WORKSPACE=${TRAVIS_BUILD_DIR}
-PYTHON_VERSION=`python -c "import sys; print('%s.%s.%s' % sys.version_info[:3])"`
 COVERAGE_CMD=coverage
 RADON_CMD=radon
 
 # Prepip scripts
-for conf in $*
+for conf in $FLAGS
 do
     file="${TRAVIS_BUILD_DIR}/src/ci_scripts/conf_scripts/${conf}-prepip.sh"
     if [[ -x "$file" ]]
@@ -32,16 +37,13 @@ pip install ${TRAVIS_BUILD_DIR}/src/dist/wirecloud*.whl
 # Install the required testing tools
 pip install django-nose mock radon
 
-DJANGO_VERSION=`django-admin.py --version`
-DJANGO_VERSION="${DJANGO_VERSION%.*}"
-
 # Create a WireCloud instance
-~/virtualenv/python$PYTHON_VERSION/bin/wirecloud-admin startproject ${WC_INSTANCE_NAME}
+wirecloud-admin startproject ${WC_INSTANCE_NAME}
 cd ${WC_INSTANCE_NAME}
 
 # And configure it
 cat ${TRAVIS_BUILD_DIR}/src/ci_scripts/base_settings.py >> ${WC_INSTANCE_NAME}/settings.py
-for conf in $*
+for conf in $FLAGS
 do
     file="${TRAVIS_BUILD_DIR}/src/ci_scripts/conf_scripts/${conf}-prepare.sh"
     if [[ -x "$file" ]]
@@ -51,10 +53,9 @@ do
 done
 
 python manage.py migrate --noinput
-python manage.py collectstatic -v 0 --noinput
+python manage.py collectstatic -v 0 -c --noinput
 
 # Pass the tests
-cd ${TRAVIS_BUILD_DIR}/src
 DJANGO_LIVE_TEST_SERVER_ADDRESS="${IP_ADDR}:28081"    # Used by Django 1.8-1.10
 DJANGO_LIVE_TEST_SERVER_HOST="${IP_ADDR}"             # Custom env variable used on Django 1.11
 ${COVERAGE_CMD} run -a --branch --source=wirecloud --omit="*/wirecloud/fp74caast/*,*/wirecloud/semanticwiring/*,*/wirecloud/guidebuilder/*,*/tests/*,*/tests.py" manage.py test --noinput --nologcapture -v 2 ${TESTS}
