0a2b9094547dd51ef72b8503f62cd7123c25abe4
Álvaro Arranz <aarranz@ficodes.com>
2018-09-11 18:57:12 +0200

    Remove django 1.8 support from documentation

--- src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
+++ src/wirecloud/commons/conf/platform_project_template/project_name/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': {{ db_engine }},       # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': {{ db_engine }},       # Add 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': {{ db_name }},           # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
