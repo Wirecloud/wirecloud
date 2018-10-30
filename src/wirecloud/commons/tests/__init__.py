5c4c20505b5fae70fd07dae34b7502e96fd67f25
Álvaro Arranz <aarranz@ficodes.com>
2018-10-30 20:10:02 +0100

    Remove resetsearchindexes command

--- src/wirecloud/commons/tests/__init__.py
+++ src/wirecloud/commons/tests/__init__.py
@@ -1,6 +1,6 @@
 from wirecloud.commons.tests.admin_commands import BaseAdminCommandTestCase, ConvertCommandTestCase, StartprojectCommandTestCase
 from wirecloud.commons.tests.basic_views import BasicViewTestCase
-from wirecloud.commons.tests.commands import ResetSearchIndexesCommandTestCase, CreateOrganizationCommandTestCase
+from wirecloud.commons.tests.commands import CreateOrganizationCommandTestCase
 from wirecloud.commons.tests.search_indexes import QueryParserTestCase, SearchAPITestCase, GroupIndexTestCase, UserIndexTestCase
 from wirecloud.commons.tests.template import TemplateUtilsTestCase
 from wirecloud.commons.tests.utils import GeneralUtilsTestCase, HTMLCleanupTestCase, WGTTestCase, HTTPUtilsTestCase
