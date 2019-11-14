14235b61a9ea8200ce467996bf6fe7b1bdc76933
Álvaro Arranz <aarranz@ficodes.com>
2019-11-14 22:58:10 +0100

    Improve tooltip placement and initial unit tests

--- src/js_tests/styledelements/TooltipSpec.js
+++ src/js_tests/styledelements/TooltipSpec.js
@@ -1,5 +1,6 @@
 /*
  *     Copyright (c) 2016 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2019 Future Internet Consulting and Development Solutions S.L.
  *
  *     This file is part of Wirecloud Platform.
  *
@@ -26,7 +27,7 @@
 
     "use strict";
 
-    describe("Styled Tooltips", function () {
+    describe("Styled Tooltips", () => {
 
         var dom = null;
 
@@ -40,52 +41,215 @@
                 dom.remove();
                 dom = null;
             }
-        });
-
-        it("can be created without passing any option", function () {
-
-            var tooltip = new StyledElements.Tooltip();
-            expect(tooltip).not.toBe(null);
-
-        });
-
-        it("can be displayed and hidden just immediately", function () {
-
-            var ref_element = new StyledElements.Button();
-            var tooltip = new StyledElements.Tooltip();
-            // PhantomJS doesn't emulate CSS animations
-            // The expected behaviour when calling hide just after calling show
-            // is to find a computed opacity of 0
-            spyOn(window, 'getComputedStyle').and.returnValue({
-                getPropertyValue: function () {return "0";}
+            document.querySelectorAll('.se-tooltip').forEach((element) => {
+                element.remove();
             });
-            expect(tooltip.show(ref_element).hide().element).toBe(null);
+        });
+
+        describe("new Tooltip([options])", () => {
+
+            it("can be created without passing any option", () => {
+                var tooltip = new StyledElements.Tooltip();
+                expect(tooltip).not.toBe(null);
+            });
 
         });
 
-        it("can be forced to be hidden", function () {
+        describe("hide()", () => {
 
-            // A second call to hide should cancel current animation and hide the tooltip
-            var ref_element = new StyledElements.Button();
-            var tooltip = new StyledElements.Tooltip();
-            expect(tooltip.show(ref_element).hide().hide()).toBe(tooltip);
-            expect(tooltip.element).toBe(null);
-            expect(tooltip.visible).toBe(false);
+            afterEach(() => {
+                if ("Wirecloud" in window) {
+                    delete window.Wirecloud;
+                }
+            });
+
+            it("can be displayed and hidden just immediately", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip();
+                // The expected behaviour when calling hide just after calling show
+                // is to find a computed opacity of 0
+                spyOn(window, 'getComputedStyle').and.returnValue({
+                    getPropertyValue: function () {return "0";}
+                });
+
+                expect(tooltip.show(ref_element).hide()).toBe(tooltip);
+
+                expect(tooltip.visible).toBe(false);
+                expect(document.querySelector('.se-tooltip')).toBe(null);
+            });
+
+            it("should support WireCloud", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip();
+                tooltip.show(ref_element);
+                window.Wirecloud = {
+                    UserInterfaceManager: {
+                        _unregisterTooltip: jasmine.createSpy("_unregisterTooltip")
+                    }
+                };
+
+                expect(tooltip.hide().hide()).toBe(tooltip);
+
+                expect(window.Wirecloud.UserInterfaceManager._unregisterTooltip).toHaveBeenCalledWith(tooltip);
+            });
+
+            it("can be forced to be hidden", (done) => {
+                // A second call to hide should cancel current animation and hide the tooltip
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip();
+                tooltip.show(ref_element);
+                // Wait for CSS transitions to apply
+                setTimeout(() => {
+                    expect(tooltip.hide()).toBe(tooltip);
+                    // after first call, tooltip starts disappearing using an opacity transition
+                    // so the dom element is still there
+                    expect(tooltip.visible).toBe(true);
+                    expect(tooltip.hide()).toBe(tooltip);
+                    // second call cancels that transition removing immediately the tooltip
+                    expect(tooltip.visible).toBe(false);
+
+                    expect(document.querySelector('.se-tooltip')).toBe(null);
+                    done();
+                }, 50);
+            });
+
+            it("should do nothing when the hide method is used and the tooltip is already hidden", function () {
+                var tooltip = new StyledElements.Tooltip();
+                expect(tooltip.hide()).toBe(tooltip);
+                expect(tooltip.visible).toBe(false);
+            });
 
         });
 
-        it("should do nothing when the hide method is used and the tooltip is already hidden", function () {
+        describe("show(ref_element)", () => {
 
-            var tooltip = new StyledElements.Tooltip();
-            expect(tooltip.hide()).toBe(tooltip);
-            expect(tooltip.element).toBe(null);
-            expect(tooltip.visible).toBe(false);
+            afterEach(() => {
+                if ("Wirecloud" in window) {
+                    delete window.Wirecloud;
+                }
+            });
+
+            it("should support WireCloud", () => {
+                window.Wirecloud = {
+                    UserInterfaceManager: {
+                        _registerTooltip: jasmine.createSpy("_unregisterTooltip")
+                    }
+                };
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['right']});
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+
+                var element = document.querySelector('.se-tooltip');
+                expect(element).not.toBe(null);
+                expect(window.Wirecloud.UserInterfaceManager._registerTooltip).toHaveBeenCalledWith(tooltip);
+            });
+
+            it("second call cancels animation", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['right']});
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+
+                var element = document.querySelector('.se-tooltip');
+                expect(element).not.toBe(null);
+            });
+
+            it("support passing a bounding box", () => {
+                var ref_element = document.createElement("div");
+                var tooltip = new StyledElements.Tooltip({placement: ['right']});
+
+                expect(tooltip.show(ref_element.getBoundingClientRect())).toBe(tooltip);
+
+                var element = document.querySelector('.se-tooltip');
+                expect(element.classList.contains("se-tooltip-right")).toBe(true);
+            });
+
+            it("right placement", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['right']});
+
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+
+                var element = document.querySelector('.se-tooltip');
+                expect(element.classList.contains("se-tooltip-right")).toBe(true);
+            });
+
+            it("left placement", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['left']});
+
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+
+                var element = document.querySelector('.se-tooltip');
+                expect(element.classList.contains("se-tooltip-left")).toBe(true);
+            });
+
+            it("top placement", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['top']});
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+            });
+
+            it("bottom placement", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip({placement: ['bottom']});
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+            });
+
+            it("should manage the case where there is not space", () => {
+                var ref_element = new StyledElements.Button({text: "Test"});
+                var content = document.createElement('div');
+                content.style.width = "1400px";
+                content.style.height = "1400px";
+                content.style.background = "blue";
+                var tooltip = new StyledElements.Tooltip({
+                    content: content,
+                    placement: ['right']
+                });
+                ref_element.insertInto(dom);
+                expect(tooltip.show(ref_element)).toBe(tooltip);
+
+                const element = document.querySelector('.se-tooltip');
+                expect(element).not.toBe(null);
+                expect(element.style.top).toBe("10px");
+                expect(element.style.bottom).toBe("10px");
+                expect(element.style.right).toBe("10px");
+                expect(element.style.left).not.toBe("10px");
+                expect(element.style.left).not.toBe("");
+            });
 
         });
 
-        describe('destroy() [deprecated]', function () {
+        describe("toggle(refPosition)", () => {
 
-            it("should hide the tooltip", function () {
+            it("shows tooltips when currently hidden", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip();
+                spyOn(tooltip, "hide").and.returnValue(tooltip);
+                spyOn(tooltip, "show").and.returnValue(tooltip);
+
+                expect(tooltip.toggle(ref_element)).toBe(tooltip);
+                expect(tooltip.hide).not.toHaveBeenCalled();
+                expect(tooltip.show).toHaveBeenCalledWith(ref_element);
+            });
+
+            it("hide tooltip when currently visible", () => {
+                var ref_element = new StyledElements.Button();
+                var tooltip = new StyledElements.Tooltip();
+                tooltip.show(ref_element);
+                spyOn(tooltip, "hide").and.returnValue(tooltip);
+                spyOn(tooltip, "show").and.returnValue(tooltip);
+
+                expect(tooltip.toggle(ref_element)).toBe(tooltip);
+                expect(tooltip.hide).toHaveBeenCalledWith();
+                expect(tooltip.show).not.toHaveBeenCalled();
+            });
+
+        });
+
+        describe('destroy() [deprecated]', () => {
+
+            it("should hide the tooltip", () => {
 
                 var tooltip = new StyledElements.Tooltip();
                 spyOn(tooltip, 'hide');
--- src/wirecloud/commons/static/js/StyledElements/Tooltip.js
+++ src/wirecloud/commons/static/js/StyledElements/Tooltip.js
@@ -1,5 +1,6 @@
 /*
  *     Copyright (c) 2014-2016 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2019 Future Internet Consulting and Development Solutions S.L.
  *
  *     This file is part of Wirecloud Platform.
  *
@@ -26,39 +27,40 @@
 
     "use strict";
 
+    var privates = new WeakMap();
     var builder = new StyledElements.GUIBuilder();
-    var template = '<s:styledgui xmlns:s="http://wirecloud.conwet.fi.upm.es/StyledElements" xmlns:t="http://wirecloud.conwet.fi.upm.es/Template" xmlns="http://www.w3.org/1999/xhtml"><div class="tooltip fade" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"><t:content/></div></div></s:styledgui>';
+    var template = '<s:styledgui xmlns:s="http://wirecloud.conwet.fi.upm.es/StyledElements" xmlns:t="http://wirecloud.conwet.fi.upm.es/Template" xmlns="http://www.w3.org/1999/xhtml"><div class="se-tooltip fade" role="tooltip"><div class="se-tooltip-arrow"></div><div class="se-tooltip-inner"><t:content/></div></div></s:styledgui>';
 
-    var setPosition = function setPosition(refPosition, position) {
-        this.element.classList.remove('top', 'right', 'bottom', 'left');
+    var setPosition = function setPosition(element, refPosition, position) {
+        element.classList.remove('se-tooltip-top', 'se-tooltip-right', 'se-tooltip-bottom', 'se-tooltip-left');
 
         switch (position) {
         case 'top':
-            this.element.classList.add('top');
-            this.element.style.left = (refPosition.left + (refPosition.width - this.element.offsetWidth) / 2) + "px";
-            this.element.style.top = (refPosition.top - this.element.offsetHeight) + "px";
+            element.classList.add('se-tooltip-top');
+            element.style.left = (refPosition.left + (refPosition.width - element.offsetWidth) / 2) + "px";
+            element.style.top = (refPosition.top - element.offsetHeight) + "px";
             break;
         case 'right':
-            this.element.classList.add('right');
-            this.element.style.left = refPosition.right + "px";
-            this.element.style.top = (refPosition.top + (refPosition.height - this.element.offsetHeight) / 2) + "px";
+            element.classList.add('se-tooltip-right');
+            element.style.left = refPosition.right + "px";
+            element.style.top = (refPosition.top + (refPosition.height - element.offsetHeight) / 2) + "px";
             break;
         case 'bottom':
-            this.element.classList.add('bottom');
-            this.element.style.left = (refPosition.left + (refPosition.width - this.element.offsetWidth) / 2) + "px";
-            this.element.style.top = refPosition.bottom + "px";
+            element.classList.add('se-tooltip-bottom');
+            element.style.left = (refPosition.left + (refPosition.width - element.offsetWidth) / 2) + "px";
+            element.style.top = refPosition.bottom + "px";
             break;
         case 'left':
-            this.element.classList.add('left');
-            this.element.style.left = (refPosition.left - this.element.offsetWidth) + "px";
-            this.element.style.top = (refPosition.top + (refPosition.height - this.element.offsetHeight) / 2) + "px";
+            element.classList.add('se-tooltip-left');
+            element.style.left = (refPosition.left - element.offsetWidth) + "px";
+            element.style.top = (refPosition.top + (refPosition.height - element.offsetHeight) / 2) + "px";
             break;
         }
     };
 
-    var standsOut = function standsOut() {
-        var parent_box = this.element.parentElement.getBoundingClientRect();
-        var element_box = this.element.getBoundingClientRect();
+    var standsOut = function standsOut(element) {
+        var parent_box = element.parentElement.getBoundingClientRect();
+        var element_box = element.getBoundingClientRect();
 
         var visible_width = element_box.width - Math.max(element_box.right - parent_box.right, 0) - Math.max(parent_box.left - element_box.left, 0);
         var visible_height = element_box.height - Math.max(element_box.bottom - parent_box.bottom, 0) - Math.max(parent_box.top - element_box.top, 0);
@@ -67,86 +69,77 @@
         return element_area - visible_area;
     };
 
-    var fixPosition = function fixPosition(refPosition, weights, positions) {
+    const FIX_PLANS = {
+        "bottom": ["left", "right", "top", "bottom"],
+        "left": ["right", "top", "bottom", "left"],
+        "right": ["left", "top", "bottom", "right"],
+        "top": ["left", "right", "bottom", "top"]
+    };
+
+    var fixPosition = function fixPosition(element, refPosition, weights, positions) {
+        // Search which position has less area outside the window
         var best_weight = Math.min.apply(Math, weights);
         var index = weights.indexOf(best_weight);
         var position = positions[index];
 
-        setPosition.call(this, refPosition, position);
+        // And use it as the starting point
+        setPosition(element, refPosition, position);
 
-        var parent_box = this.element.parentElement.getBoundingClientRect();
-        var element_box = this.element.getBoundingClientRect();
+        // Reduce tooltip size to enter on the current window
+        var parent_box = element.parentElement.getBoundingClientRect();
+        var element_box = element.getBoundingClientRect();
 
-        if (element_box.bottom > parent_box.bottom) {
-            this.element.style.top = "";
-            this.element.style.bottom = "10px";
-            element_box = this.element.getBoundingClientRect();
-        }
-
-        if (element_box.top < parent_box.top) {
-            this.element.style.top = "10px";
-        }
+        var plan = FIX_PLANS[position];
+        plan.forEach((placement) => {
+            if (
+                (placement === "top" || placement === "left") && element_box[placement] < parent_box[placement]
+                || (placement === "bottom" || placement === "right") && element_box[placement] > parent_box[placement]
+            ) {
+                element.style[placement] = "10px";
+                element_box = element.getBoundingClientRect();
+            }
+        });
     };
 
     var searchBestPosition = function searchBestPosition(refPosition, positions) {
+        const priv = privates.get(this);
         var i = 0, weights = [];
-
-        do {
-            setPosition.call(this, refPosition, positions[i]);
-            weights.push(standsOut.call(this));
-            i += 1;
-        } while (weights[i - 1] > 0 && i < positions.length);
-
-        if (weights[i - 1] > 0) {
-            fixPosition.call(this, refPosition, weights, positions);
-        }
-    };
-
-    var _show = function _show(refPosition) {
-
-        if ('Wirecloud' in window) {
-            Wirecloud.UserInterfaceManager._registerTooltip(this);
-        }
-
-        if (this.visible) {
-            this.element.classList.add('in');
-            this.repaint();
-            return;
-        }
 
         if ('getBoundingClientRect' in refPosition) {
             refPosition = refPosition.getBoundingClientRect();
         }
 
-        this.element = builder.parse(template, {
-            content: this.options.content
-        }).elements[0];
-        this.element.addEventListener('transitionend', _hide.bind(this));
+        do {
+            setPosition(priv.element, refPosition, positions[i]);
+            weights.push(standsOut(priv.element));
+            i += 1;
+        } while (weights[i - 1] > 0 && i < positions.length);
 
-        var baseelement = utils.getFullscreenElement() || document.body;
-        baseelement.appendChild(this.element);
-
-        searchBestPosition.call(this, refPosition, this.options.placement);
-        this.element.classList.add('in');
-
-        return this;
+        if (weights[i - 1] > 0) {
+            fixPosition(priv.element, refPosition, weights, positions);
+        }
     };
 
     var Tooltip = function Tooltip(options) {
         var defaultOptions = {
-            'content': '',
-            'class': '',
-            'placement': ['right', 'bottom', 'left', 'top']
+            content: '',
+            class: '',
+            placement: ['right', 'bottom', 'left', 'top']
         };
-        Object.defineProperty(this, 'options', {value: utils.merge(defaultOptions, options)});
 
         StyledElements.StyledElement.call(this, []);
 
+        const priv = {
+            element: null
+        };
+        privates.set(this, priv);
         Object.defineProperties(this, {
-            element: {value: null, writable: true},
+            options: {
+                value: utils.merge(defaultOptions, options)
+            },
             visible: {
                 get: function () {
-                    return this.element != null;
+                    return priv.element != null;
                 }
             }
         });
@@ -169,18 +162,41 @@
         if (this.visible) {
             return this.hide();
         } else {
-            return _show.call(this, refElement);
+            return this.show(refElement);
         }
     };
 
-    Tooltip.prototype.show = function show(refElement) {
-        return _show.call(this, refElement);
+    Tooltip.prototype.show = function show(refPosition) {
+        const priv = privates.get(this);
+
+        if ('Wirecloud' in window) {
+            Wirecloud.UserInterfaceManager._registerTooltip(this);
+        }
+
+        if (this.visible) {
+            priv.element.classList.add('in');
+            return this.repaint();
+        }
+
+        priv.element = builder.parse(template, {
+            content: this.options.content
+        }).elements[0];
+        priv.element.addEventListener('transitionend', _hide.bind(this));
+
+        var baseelement = utils.getFullscreenElement() || document.body;
+        baseelement.appendChild(priv.element);
+
+        searchBestPosition.call(this, refPosition, this.options.placement);
+        priv.element.classList.add('in');
+
+        return this;
     };
 
     var _hide = function _hide() {
-        if (this.element != null && !this.element.classList.contains('in')) {
-            this.element.remove();
-            this.element = null;
+        const priv = privates.get(this);
+        if (priv.element != null && !priv.element.classList.contains('in')) {
+            priv.element.remove();
+            priv.element = null;
             if ('Wirecloud' in window) {
                 Wirecloud.UserInterfaceManager._unregisterTooltip(this);
             }
@@ -188,14 +204,13 @@
     };
 
     Tooltip.prototype.hide = function hide() {
-        var force;
-
         if (!this.visible) {
             return this;
         }
 
-        force = !this.element.classList.contains('in') || getComputedStyle(this.element).getPropertyValue('opacity') === "0";
-        this.element.classList.remove('in');
+        const priv = privates.get(this);
+        var force = !priv.element.classList.contains('in') || getComputedStyle(priv.element).getPropertyValue('opacity') === "0";
+        priv.element.classList.remove('in');
         if (force) {
             _hide.call(this);
         }
