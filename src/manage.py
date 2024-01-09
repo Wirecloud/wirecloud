5c73ec7459ac42267f4a439773806aae2e2db163
dmunozv04 <39565245+dmunozv04@users.noreply.github.com>
2024-01-09 12:28:28 +0100

    Collections.abc imports for Python 3.10+ compatibility with older django versions (2.0)

--- src/manage.py
+++ src/manage.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
-
+# Collections.abc imports for Python 3.10+ compatibility
 if sys.version_info.major == 3 and sys.version_info.minor >= 10:
     import collections
     from collections.abc import Callable as collectionsCallable
+    from collections.abc import Iterable as collectionsIterable
+    from collections.abc import Iterator as collectionsIterator
+    from collections.abc import Mapping as collectionsMapping
+    from collections.abc import Sequence as collectionsSequence
     collections.Callable = collectionsCallable
+    collections.Iterable = collectionsIterable
+    collections.Iterator = collectionsIterator
+    collections.Mapping = collectionsMapping
+    collections.Sequence = collectionsSequence
 
 
 if __name__ == "__main__":
